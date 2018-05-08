9488c974e8a111aaf95439418b047af161f56cb5
TIAGO OLIVOTO <tiagoolivoto@gmail.com>
2018-05-08 19:35:10 -0300

    vignnets

--- vignettes/vignnets.R
+++ vignettes/vignnets.R
@@ -9,3 +9,40 @@ require(WAASB)
 dataset = read.csv("https://data.mendeley.com/datasets/2sjz32k3s3/1/files/07764a07-172a-4285-85db-c31bc39ae480/WAASBdata.csv?dl=1")
 
 
+## ----eval = TRUE---------------------------------------------------------
+# cross-validation for AMMI model family
+AMMIweat = validation.AMMIF(dataset,
+                            resp = "GY",
+                            nboot = 10,
+                            nrepval = 2)
+
+# cross-validation for BLUP model
+BLUPweat = validation.blup(dataset,
+                            resp = "GY",
+                            nboot = 10,
+                            nrepval = 2)
+
+
+## ----echo = TRUE---------------------------------------------------------
+options(digits = 4)
+RMSEweat = rbind(AMMIweat$RMSEmean, BLUPweat$RMSEmean)
+RMSEweat = dplyr::mutate(RMSEweat, CROP = "Wheat")
+RMSEweat = RMSEweat[order(RMSEweat[,2], decreasing = F),]
+#print(RMSEweat)
+
+## ----echo = FALSE, warning = FALSE---------------------------------------
+library(kableExtra)
+options(digits = 4)
+kable(RMSEweat, "html") %>%
+  kable_styling(bootstrap_options = "striped", "condensed", full_width = F, position = "left", font_size = 12)
+
+
+## ----eval = TRUE, fig.height = 5, fig.width = 5.5, fig.align = "center"----
+# binding AMMI and BLUP RMSEs
+RMSEweat = list(RMSE = rbind(AMMIweat$RMSE, 
+                           BLUPweat$RMSE))
+# Plotting the RMSE values
+plot.validation.AMMIF(RMSEweat,
+                      violin = FALSE,
+                      col.boxplot = "gray75")
+
