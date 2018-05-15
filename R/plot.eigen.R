16ce56e077127c7fee4809856671ac4273893b55
TIAGO OLIVOTO <tiagoolivoto@gmail.com>
2018-05-15 12:39:23 -0300

    Plot the eigenvalues

--- R/plot.eigen.R
+++ R/plot.eigen.R
@@ -1,6 +1,7 @@
 plot.eigen = function(x,
                      export = FALSE,
                      file.type = "pdf",
+                     file.name = NULL,
                      width = 6,
                      height = 6,
                      size.lab = 12,
@@ -34,14 +35,20 @@ p = ggplot2::ggplot(eigen, aes(x = PC, y = Eigenvalue, group = 1))  +
   } else
 
     if(file.type == "pdf"){
-      pdf("Eigenvalues.pdf",width = width, height = height)
+      if (is.null(file.name)){
+        pdf("Eigenvalues.pdf",width = width, height = height)
+      } else
+      pdf(paste0(file.name, ".pdf"), width = width, height = height)
       plot(p)
       dev.off()
     }
 
   if (file.type == "tiff"){
+    if (is.null(file.name)){
     tiff(filename = "Eigenvalues.tiff",width = width, height = height, units = "in", compression = "lzw", res = resolution)
-    plot(p)
+    } else
+    tiff(filename = paste0(file.name, ".tiff"), width = width, height = height, units = "in", compression = "lzw", res = resolution)
+      plot(p)
     dev.off()
   }
 }
