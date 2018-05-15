18c5d53007ccb2a2e9537296f0817ce8f1fc2265
TIAGO OLIVOTO <tiagoolivoto@gmail.com>
2018-05-15 12:38:46 -0300

    Plot heat maps with genotype ranking

--- R/plot.WAASBYratio.R
+++ R/plot.WAASBYratio.R
@@ -1,19 +1,31 @@
-plot.WAASBYratio = function(x,
+plot.WAASBYratio2 = function(x,
                             type,
                             export = FALSE,
                             file.type = "pdf",
+                            file.name = NULL,
                             width = 6,
                             height = 5,
-                            labSize = 1,
+                            size.lab = 1,
                             margins = c(5, 4),
-                            y.lab = "Genotypes",
+                            y.lab = NULL,
+                            x.lab = NULL,
                             key.lab = "Genotype ranking",
                             resolution = 300,
                             ...){
   data = x
   if (type == 1){
 
+    if (is.null(x.lab)){
+      x.lab = "Number of axes"}
+    else x.lab  = x.lab
+
+    if (is.null(y.lab)){
+      y.lab = "Genotypes"}
+    else y.lab = y.lab
+
+
     if (export  ==  F|FALSE) {
+
       Rowv =  data$hetdata %>% dist %>% hclust %>% as.dendrogram %>%
         dendextend::set("branches_k_color", k = 4) %>% dendextend::set("branches_lwd", 2) %>%
         sort(type = "nodes")
@@ -29,8 +41,8 @@ plot.WAASBYratio = function(x,
                         offsetRow = -0.4,
                         offsetCol = -0.4,
                         adjCol = c(1,0.5),
-                        cexCol = labSize,
-                        cexRow = labSize,
+                        cexCol = size.lab,
+                        cexRow = size.lab,
                         trace = "none",
                         key.title = "",
                         key.xlab = key.lab,
@@ -38,14 +50,18 @@ plot.WAASBYratio = function(x,
                         key.par = list(mgp = c(1.5, 0.5, 0),
                                      mar = c(3, 0.2, 0.2, 0.2)),
                         lhei = c(1,6),
-                        xlab = "Number of axis",
+                        xlab = x.lab,
                         ylab = y.lab,
                         margins = margins)
 
     } else
 
       if (file.type == "pdf"){
+        if (is.null(file.name)){
         pdf("Heat Ranks PCA.pdf",width = width, height = height)
+        } else
+        pdf(paste0(file.name, ".pdf"), width = width, height = height)
+
         Rowv =  data$hetdata %>% dist %>% hclust %>% as.dendrogram %>%
           dendextend::set("branches_k_color", k = 4) %>% dendextend::set("branches_lwd", 2) %>%
           sort(type = "nodes")
@@ -61,8 +77,8 @@ plot.WAASBYratio = function(x,
                           offsetRow = -0.4,
                           offsetCol = -0.4,
                           adjCol = c(1,0.5),
-                          cexCol = labSize,
-                          cexRow = labSize,
+                          cexCol = size.lab,
+                          cexRow = size.lab,
                           trace = "none",
                           key.title = "",
                           key.xlab = key.lab,
@@ -70,15 +86,18 @@ plot.WAASBYratio = function(x,
                           key.par = list(mgp = c(1.5, 0.5, 0),
                                        mar = c(3, 0.2, 0.2, 0.2)),
                           lhei = c(1,6),
-                          xlab = "Number of axis",
+                          xlab = x.lab,
                           ylab = y.lab,
                           margins = margins)
         dev.off()
       }
 
     if (file.type == "tiff"){
+      if (is.null(file.name)){
       tiff(filename = "Heat Ranks PCA.tiff",width = width, height = height, units = "in", compression = "lzw", res = resolution)
-      Rowv =  data$hetdata %>% dist %>% hclust %>% as.dendrogram %>%
+      } else
+      tiff(filename = paste0(file.name, ".tiff"), width = width, height = height, units = "in", compression = "lzw", res = resolution)
+        Rowv =  data$hetdata %>% dist %>% hclust %>% as.dendrogram %>%
         dendextend::set("branches_k_color", k = 4) %>% dendextend::set("branches_lwd", 2) %>%
         sort(type = "nodes")
       Colv = data$hetdata %>% t %>% dist %>% hclust %>% as.dendrogram %>%
@@ -93,8 +112,8 @@ plot.WAASBYratio = function(x,
                         offsetRow = -0.4,
                         offsetCol = -0.4,
                         adjCol = c(1,0.5),
-                        cexCol = labSize,
-                        cexRow = labSize,
+                        cexCol = size.lab,
+                        cexRow = size.lab,
                         trace = "none",
                         key.title = "",
                         key.xlab = key.lab,
@@ -102,7 +121,7 @@ plot.WAASBYratio = function(x,
                         key.par = list(mgp = c(1.5, 0.5, 0),
                                      mar = c(3, 0.2, 0.2, 0.2)),
                         lhei = c(1,6),
-                        xlab = "Number of axis",
+                        xlab = x.lab,
                         ylab = y.lab,
                         margins = margins)
       dev.off()
@@ -111,6 +130,15 @@ plot.WAASBYratio = function(x,
   }
 
   if (type == 2){
+
+    if (is.null(x.lab)){
+      x.lab = "WAASB/GY ratio"}
+    else x.lab  = x.lab
+
+    if (is.null(y.lab)){
+      y.lab = "Genotypes"}
+    else y.lab = y.lab
+
     if (export  ==  F|FALSE) {
       Rowv = data$hetcomb %>% dist %>% hclust %>% as.dendrogram %>%
         dendextend::set("branches_k_color", k = 4) %>% dendextend::set("branches_lwd", 2)%>%
@@ -132,16 +160,21 @@ plot.WAASBYratio = function(x,
                         key.par = list(mgp = c(1.5, 0.5, 0),
                                      mar = c(3, 0.2, 0.2, 0.2)),
                         lhei = c(1,6),
-                        xlab = "WAASB/GY ratio",
+                        xlab = x.lab,
                         ylab = y.lab,
-                        cexCol = labSize,
-                        cexRow = labSize,
+                        cexCol = size.lab,
+                        cexRow = size.lab,
                         margins = margins)
 
     } else
 
       if (file.type == "pdf"){
-        pdf("Heat map Ranks WAAS-GY.pdf",width = width, height = height)
+
+        if (is.null(file.name)){
+          pdf("Heat map Ranks WAAS-GY.pdf",width = width, height = height)
+        } else
+          pdf(paste0(file.name, ".pdf"), width = width, height = height)
+
         Rowv = data$hetcomb %>% dist %>% hclust %>% as.dendrogram %>%
           dendextend::set("branches_k_color", k = 4) %>% dendextend::set("branches_lwd", 2)%>%
           dendextend::rotate_DendSer(ser_weight = dist(data$hetcomb))%>%
@@ -162,16 +195,20 @@ plot.WAASBYratio = function(x,
                           key.par = list(mgp = c(1.5, 0.5, 0),
                                        mar = c(3, 0.2, 0.2, 0.2)),
                           lhei = c(1,6),
-                          xlab = "WAASB/GY ratio",
+                          xlab = x.lab,
                           ylab = y.lab,
-                          cexCol = labSize,
-                          cexRow = labSize,
+                          cexCol = size.lab,
+                          cexRow = size.lab,
                           margins = margins)
         dev.off()
       }
 
     if (file.type == "tiff"){
-      tiff(filename = "Heat map Ranks WAAS-GY.tiff",width = width, height = height, units = "in", compression = "lzw", res = resolution)
+      if (is.null(file.name)){
+        tiff(filename = "Heat map Ranks WAAS-GY.tiff",width = width, height = height, units = "in", compression = "lzw", res = resolution)
+      } else
+        tiff(filename = paste0(file.name, ".tiff"), width = width, height = height, units = "in", compression = "lzw", res = resolution)
+
       Rowv = data$hetcomb %>% dist %>% hclust %>% as.dendrogram %>%
         dendextend::set("branches_k_color", k = 4) %>% dendextend::set("branches_lwd", 2)%>%
         dendextend::rotate_DendSer(ser_weight = dist(data$hetcomb))%>%
@@ -192,10 +229,10 @@ plot.WAASBYratio = function(x,
                         key.par = list(mgp = c(1.5, 0.5, 0),
                                      mar = c(3, 0.2, 0.2, 0.2)),
                         lhei = c(1,6),
-                        xlab = "WAASB/GY ratio",
+                        xlab = x.lab,
                         ylab = y.lab,
-                        cexCol = labSize,
-                        cexRow = labSize,
+                        cexCol = size.lab,
+                        cexRow = size.lab,
                         margins = margins)
       dev.off()
     }
