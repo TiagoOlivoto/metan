d62222c3d60911aed93ba930e34f02892d7f5cc3
TIAGO OLIVOTO <tiagoolivoto@gmail.com>
2018-10-05 08:15:13 -0300

    A personalized theme for the ggplot2-based graphics

---
+++ R/theme_waasb.R
@@ -0,0 +1,15 @@
+theme_waasb = function () {
+  theme_bw() %+replace% # allows the entered values to be overwritten
+    theme(axis.ticks.length = unit(.2, "cm"),
+          axis.text = element_text(size = 14, colour = "black"),
+          axis.title = element_text(size = 14, colour = "black"),
+          axis.ticks = element_line(colour = "black"),
+          legend.position = c(0.85, 0.1), # bottom right
+          plot.margin = margin(0.1, 0.1, 0.1, 0.1, "cm"),
+          legend.title = element_blank(),
+          legend.text = element_text(size = 12),
+          panel.border = element_rect(colour = "black", fill = NA, size = 1),
+          panel.grid.major.x = element_blank(), panel.grid.major.y = element_blank(),
+          panel.grid.minor.x = element_blank(), panel.grid.minor.y = element_blank(),
+          aspect.ratio = 1)
+}
