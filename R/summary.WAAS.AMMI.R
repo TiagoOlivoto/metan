bfdc1604df5c23a4512ea52f549958ac633e7af3
TIAGO OLIVOTO <tiagoolivoto@gmail.com>
2018-10-08 10:19:54 -0300

    S3 method "summary" for the classes WAASB e WAAS.AMMI

--- R/summary.WAAS.AMMI.R
+++ R/summary.WAAS.AMMI.R
@@ -1,12 +1,12 @@
-summary.WAASB = function(object,
+summary.WAAS.AMMI = function(object,
                          export = FALSE,
                          file.name = NULL,
                          digits = 4,
                          ...){
 
   class = class(object)
-  if(!class == "WAASB"){
-    stop("The object must be of class 'WAASB'")
+  if(!class == "WAAS.AMMI"){
+    stop("The object must be of class 'WAAS.AMMI' or 'WAASB'")
   }
 
   options(digits = digits)
@@ -21,31 +21,13 @@ summary.WAASB = function(object,
   cat("\nIndividual analysis\n")
   print.data.frame(object$individual$individual, row.names = FALSE)
   cat("\n")
-  cat("\nFixed effects\n")
-  print.data.frame(object$REML$fixed, row.names = FALSE)
-  cat("\n")
-  cat("\nRandom effects\n")
-  print.data.frame(object$REML$random, row.names = FALSE)
-  cat("\n")
-  cat("\nStatistics\n")
-  print.data.frame(object$REML$statistics, row.names = FALSE)
-  cat("\n")
-  cat("\nSome estimates\n")
-  print.data.frame(object$ESTIMATES, row.names = FALSE)
+  cat("----------------------------------------------------------- AMMI analysis ---------------------------------------------------\n")
+  print.data.frame(object$anova)
   cat("\n")
   cat("--------------------------------------------- Weighted average of the absolute scores ----------------------------------------\n")
   print.data.frame(object$model, row.names = FALSE)
   cat("\n")
-  cat("---------------------------------- Principal component analysis of the G x E interaction matrix-------------------------------\n")
-  print.data.frame(object$PCA, row.names = FALSE)
-  cat("\n")
-  cat("------------------------------------------------------- BLUPs for genotypes ---------------------------------------------------\n")
-  print.data.frame(object$BLUPgen, row.names = FALSE)
-  cat("\n")
-  cat("----------------------------------------------- BLUPs for genotypes-vs-environments-------------------------------------------\n")
-  print.data.frame(object$BLUPgge, row.names = FALSE)
-  cat("\n")
-  cat("-------------------------------------------- Average of Genotypes in the environments -----------------------------------------\n")
+  cat("----------------------------------------------- Means for genotypes-vs-environments -------------------------------------------\n")
   print.data.frame(object$MeansGxE, row.names = FALSE)
   cat("\n")
   cat("-------------------------------------------- Some information regarding the analysis -----------------------------------------\n")
@@ -55,8 +37,8 @@ summary.WAASB = function(object,
   cat("------------------------------------------------------ End of data -----------------------------------------------------------\n")
 
   if(export  ==  TRUE|T){
-    if(is.null(file.name) == T){
-      file.name = "WAASB summary"
+    if(is.null(file.name)==T){
+      file.name = "WAAS.AMMI Summary"
     } else{file.name = file.name}
     sink(paste0(file.name,".txt"))
     options(max.print = 99999999, width = 130)
@@ -67,39 +49,17 @@ summary.WAASB = function(object,
     cat("PctWAAS = Percentage of the i-th observation in relation to the minimum WAAS ","\n")
     cat("WAASY = Weighted average of Absolute Scores and Yield (this index allow weighting the productivity and the stability) ","\n")
     cat("-----------------------------------------------------------------------------------------------------------------------------\n")
-    cat("-------------------------------------------------------- Summary of analysis ------- ----------------------------------------\n")
+    cat("------------------------------------------------- Summary of analysis of variance--- ----------------------------------------\n")
     cat("\nIndividual analysis\n")
     print.data.frame(object$individual$individual, row.names = FALSE)
     cat("\n")
-    cat("\nFixed effects\n")
-    cat("----------------------------------------------------------\n")
-    print.data.frame(object$REML$fixed, row.names = FALSE)
-    cat("\n")
-    cat("\nRandom effects\n")
-    cat("----------------------------------------------------------\n")
-    print.data.frame(object$REML$random, row.names = FALSE)
-    cat("\n")
-    cat("\nStatistics\n")
-    cat("----------------------------------------------------------\n")
-    print.data.frame(object$REML$statistics, row.names = FALSE)
-    cat("\n")
-    cat("\nSome estimates\n")
-    cat("----------------------------------------------------------\n")
-    print.data.frame(object$ESTIMATES, row.names = FALSE)
+    cat("----------------------------------------------------------- AMMI analysis ---------------------------------------------------\n")
+    print.data.frame(object$anova)
     cat("\n")
     cat("--------------------------------------------- Weighted average of the absolute scores ----------------------------------------\n")
     print.data.frame(object$model, row.names = FALSE)
     cat("\n")
-    cat("---------------------------------- Principal component analysis of the G x E interaction matrix-------------------------------\n")
-    print.data.frame(object$PCA, row.names = FALSE)
-    cat("\n")
-    cat("------------------------------------------------------- BLUPs for genotypes ---------------------------------------------------\n")
-    print.data.frame(object$BLUPgen, row.names = FALSE)
-    cat("\n")
-    cat("----------------------------------------------- BLUPs for genotypes-vs-environments-------------------------------------------\n")
-    print.data.frame(object$BLUPgge, row.names = FALSE)
-    cat("\n")
-    cat("-------------------------------------------- Average of Genotypes in the environments -----------------------------------------\n")
+    cat("----------------------------------------------- Means for genotypes-vs-environments -------------------------------------------\n")
     print.data.frame(object$MeansGxE, row.names = FALSE)
     cat("\n")
     cat("-------------------------------------------- Some information regarding the analysis -----------------------------------------\n")
@@ -109,7 +69,6 @@ summary.WAASB = function(object,
     cat("------------------------------------------------------ End of data -----------------------------------------------------------\n")
     sink()
   }
-
 }
 
 
