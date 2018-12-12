ad38ada3969e4ded445c341ab3d65e4f0e29d420
TIAGO OLIVOTO <tiagoolivoto@gmail.com>
2018-12-12 13:51:54 -0200

    stability indexes proposed by Resende

---
+++ R/resende_indexes.R
@@ -0,0 +1,34 @@
+resende_indexes =  function(x){
+x = x$BLUPgge
+# Helper functions
+hmean_fun = function(x){
+hmean = length(x)/sum(1/x)
+return(hmean)
+}
+make_matrix = function(data, row, col, value){
+  nam = cbind(c(row, col, value))
+  data = data.frame(data[(match(c(nam), names(data)))])
+  return(data.frame(tapply(data[, 3], data[, c(1, 2)], mean)))
+}
+# Harmonic mean
+GEPRED = make_matrix(x, "GEN", "ENV", "Predicted")
+HMGV = data.frame(HMGV = apply(GEPRED, 1, FUN = hmean_fun),
+                  HMGV_order = rank(-apply(GEPRED, 1, FUN = hmean_fun)))
+## Relative performance
+y = model_mil_2$MeansGxE
+GEMEAN = make_matrix(y, "GEN", "ENV", "Y")
+ovmean = mean(y$Y)
+mean_env = apply(GEMEAN, 2, FUN = mean)
+RPGV = data.frame(RPGV = apply(t(t(GEPRED) / mean_env), 1, mean))
+RPGV_data = dplyr::mutate(RPGV,
+                          GY_RPGV = RPGV * ovmean,
+                          RPGV_order = rank(-GY_RPGV))
+## Harmonic mean of Relative performance
+HMRPGV = data.frame(HMRPGV = apply(t(t(GEPRED) / mean_env), 1, hmean_fun))
+HMRPGV_data = dplyr::mutate(HMRPGV,
+                            GY_HMRPGV = HMRPGV * ovmean,
+                            HMRPGV_order = rank(-GY_HMRPGV))
+final = data.frame(cbind(HMGV, RPGV_data, HMRPGV_data))
+rownames(final) = rownames(RPGV)
+return(final)
+}
