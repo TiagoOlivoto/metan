f246c640a670cfe3bdcc3d327865e860a470b519
TIAGO OLIVOTO <tiagoolivoto@gmail.com>
2019-01-07 10:57:04 -0200

    Default theme for ggplot-based plots

--- R/theme_waasb.R
+++ R/theme_waasb.R
@@ -1,14 +1,10 @@
 theme_waasb = function () {
   theme_bw() %+replace% # allows the entered values to be overwritten
     theme(axis.ticks.length = unit(.2, "cm"),
-          axis.text = element_text(size = size.tex.lab, colour = "black"),
-          axis.title = element_text(size = size.tex.lab, colour = "black"),
           axis.ticks = element_line(colour = "black"),
           legend.position = c(0.85, 0.1), # bottom right
           plot.margin = margin(0.1, 0.1, 0.1, 0.1, "cm"),
-          plot.title = element_text(size = size.text.pa, hjust = 0, vjust = 1),
           legend.title = element_blank(),
-          legend.text = element_text(size = 10),
           panel.border = element_rect(colour = "black", fill = NA, size = 1),
           panel.grid.major.x = element_blank(), panel.grid.major.y = element_blank(),
           panel.grid.minor.x = element_blank(), panel.grid.minor.y = element_blank())
