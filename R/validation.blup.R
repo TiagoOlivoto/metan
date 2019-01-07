ad951a5d9af773efafafb865b26533c821dcf228
TIAGO OLIVOTO <tiagoolivoto@gmail.com>
2019-01-07 14:24:32 -0200

    Cross-validation for BLUP prediction

--- R/validation.blup.R
+++ R/validation.blup.R
@@ -15,6 +15,7 @@ Y = eval(substitute(resp), eval(data))
 GEN = factor(eval(substitute(gen), eval(data)))
 ENV = factor(eval(substitute(env), eval(data)))
 REP = factor(eval(substitute(rep), eval(data)))
+REPS = eval(substitute(rep), eval(data))
 data = data.frame(ENV, GEN, REP, Y)
 data = mutate(data, ID = rownames(data))
 Nbloc = length(unique(REP))
@@ -38,7 +39,7 @@ for (b in 1:nboot) {
   for (K in 1:Nenv){
     X = sample(1:10000,1)
     set.seed(X)
-    X2 = sample(1:Nbloc,nrepval , replace = F)
+    X2 = sample(unique(REPS), nrepval, replace = F)
     names = factor(data$ENV)
     names = levels(names)[actualenv + 1]
     actualenv = actualenv + 1
