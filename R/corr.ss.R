149987031298ebaa07528de068c0dd88442654c6
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-03-04 15:22:20 -0300

    Confidence interval and sample size for Pearson correlation

--- R/corr.ss.R
+++ R/corr.ss.R
@@ -1,11 +1,9 @@
-corr.ss = function(r, CI) {
-  n=(CI/(0.45304^r*2.25152))^(1/-0.50089)
-  cat("-------------------------------------------------","\n")
-  cat("Sample size planning for correlation coefficient","\n")
-  cat("-------------------------------------------------","\n")
-  cat(paste0("Level of significance: 5%",
-                                         "\nCorrelation coefficient: ", r,
-                                         "\n95% half-width CI: ", CI,
-                                         "\nRequired sample size: ", round(n,0)),"\n")
-  cat("-------------------------------------------------","\n")
+corr.ss <- function(r, CI) {
+    n <- (CI/(0.45304^r * 2.25152))^(1/-0.50089)
+    cat("-------------------------------------------------", "\n")
+    cat("Sample size planning for correlation coefficient", "\n")
+    cat("-------------------------------------------------", "\n")
+    cat(paste0("Level of significance: 5%", "\nCorrelation coefficient: ", r, "\n95% half-width CI: ", 
+        CI, "\nRequired sample size: ", round(n, 0)), "\n")
+    cat("-------------------------------------------------", "\n")
 }
