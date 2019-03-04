835bd4422ec7c602780de02fb2ff34b2d8de48bb
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-03-04 15:25:48 -0300

    Updated

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,11 @@
-if(getRversion() >= "2.15.1") {
-  utils::globalVariables(c("..density..", ".fitted", ".resid", ".scresid", "Accumul",
-                           "Accumulated", "BLUPe", "BLUPg", "BLUPge", "CI", "Code",
-                           "ENV", "Eigenvalue", "FAI", "GEN", "GY_HMRPGV", "GY_RPGV",
-                           "Genotype", "MODEL", "Mean", "PC", "PC1", "PC2", "Parameters",
-                           "PctResp", "PctWAAS", "PctWAASB", "Percent", "PesRes", "PesWAAS",
-                            "PesWAASB", "Predicted", "Proportion", "RMSPD", "Rank", "ResAMMI",
-                            "WAAS", "WAASBY", "WAASY", "X1", "X2", "Y", "Ypred",
-                            "YpredAMMI", "envPC1", "factors", "gen", "genPC1",
-                            "ggee", "id", "label", "nominal", "pred", "resOLS", "sel",
-                             "stdres", "type", "y", "z", "Corr", "combn", "cov2cor", "partial", "pt"))
+if (getRversion() >= "2.15.1") {
+    utils::globalVariables(c("..density..", ".fitted", ".resid", ".scresid", "Accumul", 
+        "Accumulated", "BLUPe", "BLUPg", "BLUPge", "CI", "Code", "ENV", "Eigenvalue", 
+        "FAI", "GEN", "GY_HMRPGV", "GY_RPGV", "Genotype", "MODEL", "Mean", "PC", "PC1", 
+        "PC2", "Parameters", "PctResp", "PctWAAS", "PctWAASB", "Percent", "PesRes", 
+        "PesWAAS", "PesWAASB", "Predicted", "Proportion", "RMSPD", "Rank", "ResAMMI", 
+        "WAAS", "WAASBY", "WAASY", "X1", "X2", "Y", "Ypred", "YpredAMMI", "envPC1", 
+        "factors", "gen", "genPC1", "ggee", "id", "label", "nominal", "pred", "resOLS", 
+        "sel", "stdres", "type", "y", "z", "Corr", "combn", "cov2cor", "partial", 
+        "pt"))
 }
