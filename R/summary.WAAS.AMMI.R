912e9f5add613890c62c8254b7d0b01dc7c4c397
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-03-04 15:25:27 -0300

    S3 method summary

--- R/summary.WAAS.AMMI.R
+++ R/summary.WAAS.AMMI.R
@@ -1,63 +1,62 @@
-summary.WAAS.AMMI = function(object,
-                         export = FALSE,
-                         file.name = NULL,
-                         digits = 4,
-                         ...){
-
-  class = class(object)
-  if(!class == "WAAS.AMMI"){
-    stop("The object must be of class 'WAAS.AMMI'")
-  }
-
-  if(export  ==  TRUE){
-    if(is.null(file.name)==T){
-      file.name = "WAAS.AMMI Summary"
-    } else{file.name = file.name}
-    sink(paste0(file.name,".txt"))
-    options(max.print = 99999999, width = 90, digits = digits)
-    for (i in 1:length(object)){
-      var = object[[i]]
-
-  cat("Variable", names(object)[i], "\n")
-  cat("------------------------------ Individual analysis of variance--- ---------------------\n")
-  print.data.frame(var$individual$individual, row.names = FALSE)
-  cat("\n")
-  cat("-------------------------------------- AMMI analysis -----------------------------------\n")
-  print.data.frame(var$anova)
-  cat("\n")
-  cat("------------------------- Weighted average of the absolute scores ----------------------\n")
-  print.data.frame(var$model, row.names = FALSE)
-  cat("\n")
-  cat("---------------------------- Means for genotypes-vs-environments -----------------------\n")
-  print.data.frame(var$MeansGxE, row.names = FALSE)
-  cat("\n")
-  cat("------------------------- Some information regarding the analysis ----------------------\n")
-  print.data.frame(var$Details, row.names = FALSE)
-  cat("-------------------------------------- End of data -------------------------------------\n\n\n\n")
-
+summary.WAAS.AMMI <- function(object, export = FALSE, file.name = NULL, digits = 4, 
+    ...) {
+    
+    class <- class(object)
+    if (!class == "WAAS.AMMI") {
+        stop("The object must be of class 'WAAS.AMMI'")
     }
-    sink()
-  } else {
-    for (i in 1:length(object)){
-      var = object[[i]]
-      cat("Variable", names(object)[i], "\n")
-      cat("----------------------------- Individual analysis of variance---------------------------\n")
-    print.data.frame(var$individual$individual, row.names = FALSE)
-    cat("\n")
-    cat("-------------------------------------- AMMI analysis -----------------------------------\n")
-    print.data.frame(var$anova)
-    cat("\n")
-    cat("------------------------- Weighted average of the absolute scores ----------------------\n")
-    print.data.frame(var$model, row.names = FALSE)
-    cat("\n")
-    cat("---------------------------- Means for genotypes-vs-environments -----------------------\n")
-    print.data.frame(var$MeansGxE, row.names = FALSE)
-    cat("\n")
-    cat("------------------------- Some information regarding the analysis ----------------------\n")
-    print.data.frame(var$Details, row.names = FALSE)
-    cat("------------------------------------ End of data ---------------------------------------\n")
-  }
-  }
+    
+    if (export == TRUE) {
+        if (is.null(file.name) == T) {
+            file.name <- "WAAS.AMMI Summary"
+        } else {
+            file.name <- file.name
+        }
+        sink(paste0(file.name, ".txt"))
+        options(max.print = 99999999, width = 90, digits = digits)
+        for (i in 1:length(object)) {
+            var <- object[[i]]
+            
+            cat("Variable", names(object)[i], "\n")
+            cat("------------------------------ Individual analysis of variance--- ---------------------\n")
+            print.data.frame(var$individual$individual, row.names = FALSE)
+            cat("\n")
+            cat("-------------------------------------- AMMI analysis -----------------------------------\n")
+            print.data.frame(var$anova)
+            cat("\n")
+            cat("------------------------- Weighted average of the absolute scores ----------------------\n")
+            print.data.frame(var$model, row.names = FALSE)
+            cat("\n")
+            cat("---------------------------- Means for genotypes-vs-environments -----------------------\n")
+            print.data.frame(var$MeansGxE, row.names = FALSE)
+            cat("\n")
+            cat("------------------------- Some information regarding the analysis ----------------------\n")
+            print.data.frame(var$Details, row.names = FALSE)
+            cat("-------------------------------------- End of data -------------------------------------\n\n\n\n")
+            
+        }
+        sink()
+    } else {
+        for (i in 1:length(object)) {
+            var <- object[[i]]
+            cat("Variable", names(object)[i], "\n")
+            cat("----------------------------- Individual analysis of variance---------------------------\n")
+            print.data.frame(var$individual$individual, row.names = FALSE)
+            cat("\n")
+            cat("-------------------------------------- AMMI analysis -----------------------------------\n")
+            print.data.frame(var$anova)
+            cat("\n")
+            cat("------------------------- Weighted average of the absolute scores ----------------------\n")
+            print.data.frame(var$model, row.names = FALSE)
+            cat("\n")
+            cat("---------------------------- Means for genotypes-vs-environments -----------------------\n")
+            print.data.frame(var$MeansGxE, row.names = FALSE)
+            cat("\n")
+            cat("------------------------- Some information regarding the analysis ----------------------\n")
+            print.data.frame(var$Details, row.names = FALSE)
+            cat("------------------------------------ End of data ---------------------------------------\n")
+        }
+    }
 }
 
 
--- R/summary.WAASB.R
+++ R/summary.WAASB.R
@@ -1,95 +1,91 @@
-summary.WAASB = function(object,
-                         export = FALSE,
-                         blup = FALSE,
-                         file.name = NULL,
-                         digits = 4,
-                         ...){
-
-  class = class(object)
-  if(!class == "WAASB"){
-    stop("The object must be of class 'WAASB'")
-  }
-
-  if(export  ==  TRUE){
-    if(is.null(file.name) == T){
-      file.name = "WAASB summary"
+summary.WAASB <- function(object, export = FALSE, blup = FALSE, file.name = NULL, 
+    digits = 4, ...) {
+    
+    class <- class(object)
+    if (!class == "WAASB") {
+        stop("The object must be of class 'WAASB'")
+    }
+    
+    if (export == TRUE) {
+        if (is.null(file.name) == T) {
+            file.name <- "WAASB summary"
+        } else {
+            file.name <- file.name
+        }
+        sink(paste0(file.name, ".txt"))
+        options(max.print = 99999999, width = 110)
+        for (i in 1:length(object)) {
+            var <- object[[i]]
+            cat("Variable", names(object)[i], "\n")
+            cat("----------------------- Individual fixed-model analysis of variance ---------------------\n")
+            print.data.frame(var$individual$individual, row.names = FALSE)
+            cat("\n")
+            cat("---------------------------------- Fixed effects --------------------------------------\n")
+            print.data.frame(var$fixed)
+            cat("\n")
+            cat("--------------------------------- Random effects --------------------------------------\n")
+            print.data.frame(var$random, row.names = FALSE)
+            cat("\n")
+            cat("------------------------------ Likelihood ratio test ----------------------------------\n")
+            print.data.frame(var$LRT)
+            cat("\n")
+            cat("-----------------Variance components and genetic parameters --------------------------\n")
+            print.data.frame(var$ESTIMATES, row.names = FALSE)
+            cat("\n")
+            cat("------------- Principal component analysis of the G x E interaction matrix -------------\n")
+            print.data.frame(var$PCA, row.names = FALSE)
+            cat("\n")
+            if (blup == TRUE) {
+                cat("---------------------------------- BLUPs for genotypes ---------------------------------\n")
+                print.data.frame(var$BLUPgen, row.names = FALSE)
+                cat("\n")
+                cat("--------------------------- BLUPs for genotypes-vs-environments ------------------------\n")
+                print.data.frame(var$BLUPgge, row.names = FALSE)
+                cat("\n")
+            }
+            cat("------------------------- Some information regarding the analysis ----------------------\n")
+            print.data.frame(var$Details, row.names = FALSE)
+            cat("-------------------------------------- End of data -------------------------------------\n\n\n\n")
+        }
+        sink()
     } else {
-      file.name = file.name
+        for (i in 1:length(object)) {
+            var <- object[[i]]
+            options(digits = digits)
+            cat("Variable", names(object)[i], "\n")
+            cat("-----------------------------------------------------------------------------------------\n")
+            cat("----------------------- Individual fixed-model analysis of variance ---------------------\n")
+            print.data.frame(var$individual$individual, row.names = FALSE)
+            cat("\n")
+            cat("---------------------------------- Fixed effects --------------------------------------\n")
+            print.data.frame(var$fixed)
+            cat("\n")
+            cat("--------------------------------- Random effects --------------------------------------\n")
+            print.data.frame(var$random, row.names = FALSE)
+            cat("\n")
+            cat("------------------------------ Likelihood ratio test ----------------------------------\n")
+            print.data.frame(var$LRT)
+            cat("\n")
+            cat(" -----------------Variance components and genetic parameters --------------------------\n")
+            print.data.frame(var$ESTIMATES, row.names = FALSE)
+            cat("\n")
+            cat("------------- Principal component analysis of the G x E interaction matrix -------------\n")
+            print.data.frame(var$PCA, row.names = FALSE)
+            cat("\n")
+            if (blup == TRUE) {
+                cat("---------------------------------- BLUPs for genotypes ---------------------------------\n")
+                print.data.frame(var$BLUPgen, row.names = FALSE)
+                cat("\n")
+                cat("--------------------------- BLUPs for genotypes-vs-environments ------------------------\n")
+                print.data.frame(var$BLUPgge, row.names = FALSE)
+                cat("\n")
+            }
+            cat("------------------------- Some information regarding the analysis ----------------------\n")
+            print.data.frame(var$Details, row.names = FALSE)
+            cat("-------------------------------------- End of data -------------------------------------\n")
+            
+        }
     }
-    sink(paste0(file.name,".txt"))
-    options(max.print = 99999999, width = 110)
-    for (i in 1:length(object)){
-    var = object[[i]]
-    cat("Variable", names(object)[i], "\n")
-    cat("----------------------- Individual fixed-model analysis of variance ---------------------\n")
-    print.data.frame(var$individual$individual, row.names = FALSE)
-    cat("\n")
-    cat("---------------------------------- Fixed effects --------------------------------------\n")
-    print.data.frame(var$fixed)
-    cat("\n")
-    cat("--------------------------------- Random effects --------------------------------------\n")
-    print.data.frame(var$random, row.names = FALSE)
-    cat("\n")
-    cat("------------------------------ Likelihood ratio test ----------------------------------\n")
-    print.data.frame(var$LRT)
-    cat("\n")
-    cat("-----------------Variance components and genetic parameters --------------------------\n")
-    print.data.frame(var$ESTIMATES, row.names = FALSE)
-    cat("\n")
-    cat("------------- Principal component analysis of the G x E interaction matrix -------------\n")
-    print.data.frame(var$PCA, row.names = FALSE)
-    cat("\n")
-    if (blup == TRUE){
-    cat("---------------------------------- BLUPs for genotypes ---------------------------------\n")
-    print.data.frame(var$BLUPgen, row.names = FALSE)
-    cat("\n")
-    cat("--------------------------- BLUPs for genotypes-vs-environments ------------------------\n")
-    print.data.frame(var$BLUPgge, row.names = FALSE)
-    cat("\n")
-    }
-    cat("------------------------- Some information regarding the analysis ----------------------\n")
-    print.data.frame(var$Details, row.names = FALSE)
-    cat("-------------------------------------- End of data -------------------------------------\n\n\n\n")
-  }
-    sink()
-  } else {
-    for (i in 1:length(object)){
-    var = object[[i]]
-    options(digits = digits)
-    cat("Variable", names(object)[i], "\n")
-    cat("-----------------------------------------------------------------------------------------\n")
-    cat("----------------------- Individual fixed-model analysis of variance ---------------------\n")
-    print.data.frame(var$individual$individual, row.names = FALSE)
-    cat("\n")
-    cat("---------------------------------- Fixed effects --------------------------------------\n")
-    print.data.frame(var$fixed)
-    cat("\n")
-    cat("--------------------------------- Random effects --------------------------------------\n")
-    print.data.frame(var$random, row.names = FALSE)
-    cat("\n")
-    cat("------------------------------ Likelihood ratio test ----------------------------------\n")
-    print.data.frame(var$LRT)
-    cat("\n")
-    cat(" -----------------Variance components and genetic parameters --------------------------\n")
-    print.data.frame(var$ESTIMATES, row.names = FALSE)
-    cat("\n")
-    cat("------------- Principal component analysis of the G x E interaction matrix -------------\n")
-    print.data.frame(var$PCA, row.names = FALSE)
-    cat("\n")
-    if (blup == TRUE){
-      cat("---------------------------------- BLUPs for genotypes ---------------------------------\n")
-      print.data.frame(var$BLUPgen, row.names = FALSE)
-      cat("\n")
-      cat("--------------------------- BLUPs for genotypes-vs-environments ------------------------\n")
-      print.data.frame(var$BLUPgge, row.names = FALSE)
-      cat("\n")
-    }
-    cat("------------------------- Some information regarding the analysis ----------------------\n")
-    print.data.frame(var$Details, row.names = FALSE)
-    cat("-------------------------------------- End of data -------------------------------------\n")
-
-  }
-}
 }
 
 
