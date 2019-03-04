2e2a250b17207eff93dc34cbe76da73443b32c0a
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-03-04 15:24:32 -0300

    Reescale a continuous variable

--- R/resca.R
+++ R/resca.R
@@ -1,8 +1,6 @@
-resca = function(values,
-                 new_min = 0,
-                 new_max = 100){
-new_v = function(v){
-(new_max - new_min) / (max(values) - min(values)) * (v - max(values)) + new_max
-}
-return(sapply(values, new_v))
+resca <- function(values, new_min = 0, new_max = 100) {
+    new_v <- function(v) {
+        (new_max - new_min)/(max(values) - min(values)) * (v - max(values)) + new_max
+    }
+    return(sapply(values, new_v))
 }
