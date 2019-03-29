a12bacf0fad3d6e4318672bbfa211a1038cbcd56
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-03-28 23:53:01 -0300

    AMMI-based stability indexes

--- R/AMMI_indexes.R
+++ R/AMMI_indexes.R
@@ -1,23 +1,23 @@
-AMMI_indexes <- function(x, order.y = NULL) {
-    
+AMMI_indexes <- function(.data, order.y = NULL) {
+
     if (is.null(order.y)) {
-        order.y <- rep("h", length(x))
+        order.y <- rep("h", length(.data))
     }
-    if (!is(x, "WAAS.AMMI")) {
+    if (!is(.data, "WAAS.AMMI")) {
         stop("The object 'x' must be an object of class \"WAAS.AMMI\"")
     }
     if (any(!order.y %in% c("h", "l")) == TRUE) {
         stop("The arguments in 'order.y' must be one of the 'h' or 'l'")
     }
-    if (length(order.y) != length(x)) {
+    if (length(order.y) != length(.data)) {
         stop("The lenght of argument 'order.y' must be equal the length of 'x'")
     }
     listres <- list()
     varin <- 1
-    for (var in 1:length(x)) {
-        
-        model <- x[[var]]
-        
+    for (var in 1:length(.data)) {
+
+        model <- .data[[var]]
+
         n <- sum(model$PCA$`Pr(>F)` <= 0.05, na.rm = TRUE)
         meange <- model$MeansGxE
         effects <- residuals(lm(Y ~ ENV + GEN, data = meange))
@@ -27,7 +27,7 @@ AMMI_indexes <- function(x, order.y = NULL) {
         svdge <- svd(ge)
         gamma.n <- svdge$u[, 1:n]
         theta.n <- model$PCA$Percent[1:n]/100
-        
+
         # sum of absolute scores
         PCA <- data.frame(model$model)
         SCOR <- PCA[PCA[, 1] == "GEN", c(seq(4, n + 3))]
@@ -38,7 +38,7 @@ AMMI_indexes <- function(x, order.y = NULL) {
         }
         mean <- PCA[PCA[, 1] == "GEN", c(2:3)]
         rS <- rank(SIPC)
-        
+
         if (length(order.y) == 1) {
             if (order.y == "h") {
                 rY <- rank(-mean[2])
@@ -56,8 +56,8 @@ AMMI_indexes <- function(x, order.y = NULL) {
         }
         varin <- varin + 1
         ssiSIPC <- rS + rY
-        
-        
+
+
         # Za
         if (n == 1) {
             Za <- abs(gamma.n * theta.n)
@@ -66,8 +66,8 @@ AMMI_indexes <- function(x, order.y = NULL) {
         }
         rZA <- rank(Za)
         ssiZA <- rZA + rY
-        
-        
+
+
         # averages of the squared eigenvector
         if (n == 1) {
             EV <- gamma.n^2/n
@@ -76,22 +76,22 @@ AMMI_indexes <- function(x, order.y = NULL) {
         }
         rEV <- rank(EV)
         ssiEV <- rEV + rY
-        
+
         # AMMI stability values
         pc <- model$anova$`Sum Sq`[5]/model$anova$`Sum Sq`[6]
         SCOR2 <- PCA[PCA[, 1] == "GEN", c(seq(4, 2 + 3))]
         ASV <- sqrt((pc * SCOR2[, 1])^2 + SCOR2[, 2]^2)
         rASV <- rank(ASV)
         ssiASV <- rASV + rY
-        
-        temp <- data.frame(GEN = mean[1], Y = mean[2], rY = rY, ASV = ASV, rASV = rASV, 
-            ssiASV = ssiASV, SIPC = SIPC, rSIPC = rS, ssiSIPC = ssiSIPC, EV = EV, 
+
+        temp <- data.frame(GEN = mean[1], Y = mean[2], rY = rY, ASV = ASV, rASV = rASV,
+            ssiASV = ssiASV, SIPC = SIPC, rSIPC = rS, ssiSIPC = ssiSIPC, EV = EV,
             rEV = rEV, ssiEV = ssiEV)
-        
-        listres[[paste(names(x[var]))]] <- temp
-        
+
+        listres[[paste(names(.data[var]))]] <- temp
+
     }
-    
+
     invisible(listres)
 }
 
