c34d8a4e24c0d595d52b360ca6b20443a3a10b4d
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-04-04 23:58:18 -0300

    Make a matrix of means based on two factors and one numerical variable

--- R/mmat.R
+++ R/mmat.R
@@ -1,5 +1,7 @@
-mmat <- function(data, row, col, value) {
-    data <- data.frame(R = eval(substitute(row), eval(data)), C = eval(substitute(col), 
-        eval(data)), V = eval(substitute(value), eval(data)))
-    return(data.frame(tapply(data[, 3], data[, c(1, 2)], mean)))
+mmat <- function(.data, row, col, value) {
+  data = data.frame(.data %>% dplyr::select(!!dplyr::enquo(row),
+                                            !!dplyr::enquo(col),
+                                            !!dplyr::enquo(value)))
+  return(data.frame(tapply(data[, 3], data[, c(1, 2)], mean)))
 }
+
