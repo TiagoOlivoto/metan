91b67991e43f824600c4b070ef7b13f20c595d0f
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-04-14 02:23:18 -0300

    Helper function for combining data.frames by row, filling missing values

--- R/rbind_fill.R
+++ R/rbind_fill.R
@@ -1,9 +1,9 @@
-rbind_fill <- function(...) {
+rbind_fill <- function(..., fill = ".") {
     df <- list(...)
     ns <- unique(unlist(sapply(df, names)))
     do.call(rbind, lapply(df, function(x) {
         for (n in ns[!ns %in% names(x)]) {
-            x[[n]] <- "."
+            x[[n]] <- fill
         }
         x
     }))
