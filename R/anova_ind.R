d3e0d53f276dd42cdddbc5a9f37b1844432c104b
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-04-14 01:15:42 -0300

    Within-environment analysis of variance

--- R/anova_ind.R
+++ R/anova_ind.R
@@ -2,17 +2,26 @@ anova_ind = function(.data,
                      env,
                      gen,
                      rep,
-                     resp){
-  group_var <- dplyr::enquo(env)
-  genot <- dplyr::enquo(gen)
-  replicate <- dplyr::enquo(rep)
-  resp <- dplyr::enquo(resp)
-  grouped =  .data  %>%
-    dplyr::select(!!group_var, !!genot, !!replicate, !!resp) %>%
-    dplyr::group_by(!!!group_var, !!!genot, !!!replicate) %>%
-    dplyr::summarise(mean = mean(!!resp)) %>%
-    dplyr::ungroup() %>%
-    split(dplyr::pull(., !!group_var))
+                     resp,
+                     verbose = TRUE){
+  datain <- .data
+  GEN <- factor(eval(substitute(gen), eval(datain)))
+  ENV <- factor(eval(substitute(env), eval(datain)))
+  REP <- factor(eval(substitute(rep), eval(datain)))
+  listres <- list()
+  d <- match.call()
+  nvar <- as.numeric(ifelse(length(d$resp) > 1, length(d$resp) - 1, length(d$resp)))
+  for (var in 2:length(d$resp)) {
+    if (length(d$resp) > 1) {
+      Y <- eval(substitute(resp)[[var]], eval(datain))
+      varnam = paste(d$resp[var])
+    } else {
+      Y <- eval(substitute(resp), eval(datain))
+      varnam = paste(d$resp)
+    }
+    data <- data.frame(ENV, GEN, REP, Y)
+    names(data) = c("ENV", "GEN", "REP", "mean")
+  grouped =  data  %>% split(dplyr::pull(., ENV))
   formula = as.formula(paste0("mean ~ ", substitute(gen), "+", substitute(rep)))
   individual = do.call(rbind,
                        lapply(grouped, function(x){
@@ -35,7 +44,18 @@ anova_ind = function(.data,
                                                   AS = AS))
 
                        }))
-  return(list(individual = individual,
-              MSRratio = max(individual$MSR)/min(individual$MSR)))
+  temp = list(individual = individual,
+              MSRratio = max(individual$MSR)/min(individual$MSR))
+  if (length(d$resp) > 1) {
+    listres[[paste(d$resp[var])]] <- temp
+    if (verbose == TRUE) {
+      cat("Evaluating variable", paste(d$resp[var]), round((var - 1)/(length(d$resp) -
+                                                                        1) * 100, 1), "%", "\n")
+    }
+  } else {
+    listres[[paste(d$resp)]] <- temp
+  }
+  }
+  return(listres)
 }
 
