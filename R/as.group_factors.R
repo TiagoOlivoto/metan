b57a6d7f6d2233e5190f5c710853dab387150785
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-04-14 11:07:43 -0300

    Coerce to an object of class group_factors

--- R/as.group_factors.R
+++ R/as.group_factors.R
@@ -1,8 +1,11 @@
-as.group_factors = function(x) {
+as.group_factors = function(x, verbose = TRUE) {
   grouped <- group_by_if(x, is.factor)
   names <- rlang::eval_bare(rlang::expr(paste(!!!group_keys(grouped), sep = " / ")))
   gd =   grouped %>%
     group_split(keep = FALSE) %>%
     rlang::set_names(names)
+  if (verbose == TRUE){
+    message("The data was splitted up considering the factors", paste0(collapse = " ", names(x[ , unlist(lapply(x, is.factor)) ])))
+  }
   return(structure(gd, class = "group_factors"))
 }
