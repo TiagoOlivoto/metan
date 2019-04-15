2cfd3e9e0a2124743018457248f865cf143aaccc
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-04-14 23:55:01 -0300

    Split a dataframe into subsets grouped by one or more factors

--- R/group_factors.R
+++ R/group_factors.R
@@ -1,18 +1,24 @@
-group_factors = function(.data, ..., keep_factors = FALSE) {
+group_factors = function(.data, ..., keep_factors = FALSE, verbose = TRUE) {
   grouped <- group_by(.data, ...)
   names <- rlang::eval_bare(rlang::expr(paste(!!!group_keys(grouped), sep = " / ")))
   gd =   grouped %>%
     group_split(keep = FALSE) %>%
     rlang::set_names(names)
   if (keep_factors == FALSE){
+    if (verbose == TRUE){
       message("The factors ", paste0(collapse = " ", names(gd[[1]][ , unlist(lapply(gd[[1]], is.factor)) ])),
             " where ignored. Use 'keep_factors = TRUE' to keep this columns in the grouped data. ")
+    }
     gd = lapply(gd, function(x){
       x[ , unlist(lapply(x, is.numeric))] %>% as.data.frame()
     })
   } else{
     gd = lapply(gd, function(x){
       x %>% as.data.frame()})
+    if (verbose == TRUE){
+      message("The factors ", paste0(collapse = " ", names(gd[[1]][ , unlist(lapply(gd[[1]], is.factor)) ])),
+              " where ignored. Use 'keep_factors = TRUE' to keep this columns in the grouped data. ")
+    }
   }
   return(structure(gd, class = "group_factors"))
 }
