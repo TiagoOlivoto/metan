d0973c942dc5fc33d28169e70aac3c25256d4f8c
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-04-20 23:42:03 -0300

    Coerce to an object of class lpcor

--- R/as.lpcorr.R
+++ R/as.lpcorr.R
@@ -1,8 +1,5 @@
-as.lpcor = function(x, y, ...){
-  data = list(x, y, ...)
-  if(length(data)== 1){
-    stop(call. = FALSE, "At least two matrices must be used")
-  }
+as.lpcor = function(...){
+  data = list(...)
   if(length(which(sapply(data, function(x) identical(nrow(x), ncol(x)))==TRUE)) != length(data)){
     stop(call. = FALSE, "All matrices in the list must be a square matrix. Please, check and fix.")
   }
@@ -10,6 +7,7 @@ as.lpcor = function(x, y, ...){
     stop(call. = FALSE, "All matrices in the list must be the same dimension. Please, check and fix.")
   }
   data =  lapply(data, function(x) as.matrix(x))
+  names(data) = paste("mat", 1:length(data), sep = "_")
   invisible(structure(data, class = "lpcor"))
 }
 
