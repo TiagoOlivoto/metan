7eb4b8fb53180730fc1d3f89cd46cf8906bb841a
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-05-06 15:16:09 -0300

    Canonical correlation analysis

--- R/can_cor.R
+++ R/can_cor.R
@@ -1,5 +1,6 @@
 can_corr = function(.data = NULL, FG = NULL, SG = NULL, use = "cor",
-  test = "Bartlett", prob = 0.05, center = TRUE, verbose = TRUE, collinearity = TRUE) {
+                    test = "Bartlett", prob = 0.05, center = TRUE, verbose = TRUE,
+                    collinearity = TRUE) {
   if (missing(.data) & missing(FG) || missing(SG)) {
     stop("No valid data imput for analysis.")
   }
@@ -9,11 +10,6 @@ can_corr = function(.data = NULL, FG = NULL, SG = NULL, use = "cor",
   if (!use %in% c("cov", "cor")) {
     stop("The argument  'use' is incorrect, it should be 'cov' or 'cor'.")
   }
-  if (!missing(.data)) {
-    FG = as.data.frame(dplyr::select(.data, !!!dplyr::quos(!!dplyr::enquo(FG))))
-    SG = as.data.frame(dplyr::select(.data, !!!dplyr::quos(!!dplyr::enquo(SG))))
-  }
-
   if (missing(.data) & !missing(FG) & !missing(SG)) {
     if (!is.data.frame(FG)) {
       stop("'FG' should be data frame.")
@@ -21,6 +17,214 @@ can_corr = function(.data = NULL, FG = NULL, SG = NULL, use = "cor",
     if (!is.data.frame(SG)) {
       stop("'SG' should be data frame.")
     }
+  }
+  if (any(class(.data) == "group_factors")) {
+    dfs = list()
+    datain = .data
+    for (k in 1:length(.data)) {
+      .data = datain[[k]]
+      nam = names(datain[k])
+      FGV = as.data.frame(dplyr::select(.data, !!!dplyr::quos(!!dplyr::enquo(FG))))
+      SGV = as.data.frame(dplyr::select(.data, !!!dplyr::quos(!!dplyr::enquo(SG))))
+      if (nrow(FGV) != nrow(SGV)) {
+        stop("The number of observations of 'FG', should be equal to 'SG'.")
+      }
+      if (ncol(FGV) > ncol(SGV)) {
+        stop("The number of variables in 'FG' should be lesser than or equal to the number of variables in 'SG'.")
+      }
+      if (!test %in% c("Bartlett", "Rao")) {
+        stop("The argument 'test' is incorrect, it should be 'Bartlett' or 'Rao'.")
+      }
+      if (!is.numeric(prob) | prob <= 0 || prob > 1) {
+        stop("The argument 'prob' is incorrect. It should be numeric with values between 0 and 1.")
+      }
+
+      if (use == "cov") {
+        MC = cov(cbind(FGV, SGV))
+        S11 = cov(FGV)
+        S22 = cov(SGV)
+        S12 = cov(FGV, SGV)
+        S21 = cov(SGV, FGV)
+      }
+      if (use == "cor") {
+        MC = cor(cbind(FGV, SGV))
+        S11 = cor(FGV)
+        S22 = cor(SGV)
+        S12 = cor(FGV, SGV)
+        S21 = cor(SGV, FGV)
+      }
+      M1 <- eigen(S11)
+      megval1 <- M1$values
+      megvec1 <- M1$vectors
+      S11_12 = megvec1 %*% diag(1/sqrt(megval1)) %*% t(megvec1)
+      S22_Inv = solve(S22)
+      M2 <- eigen(S11_12 %*% S12 %*% S22_Inv %*% S21 %*%
+                    S11_12)
+      megval2 <- M2$values
+      megvec2 <- M2$vectors
+      mtr = megval2
+      varuv <- as.data.frame(matrix(NA, length(mtr), 3))
+      rownames(varuv) <- paste("U", 1:length(mtr), "V",
+                               1:length(mtr), sep = "")
+      colnames(varuv) <- c("Variance", "Proportion", "Cum_proportion")
+      varuv[, "Variance"] <- mtr
+      varuv[, "Proportion"] <- (mtr/sum(mtr)) * 100
+      varuv[, "Cum_proportion"] <- cumsum(varuv[, "Proportion"])
+      coruv <- as.matrix(sqrt(mtr), ncol = length(coruv),
+                         nrow = 1)
+      rownames(coruv) <- paste("U", 1:length(coruv), "V",
+                               1:length(coruv), sep = "")
+      colnames(coruv) <- c("Correlation")
+      Coef_FG = S11_12 %*% megvec2
+      rownames(Coef_FG) <- colnames(FGV)
+      colnames(Coef_FG) <- paste("U", 1:ncol(Coef_FG),
+                                 sep = "")
+      Coef_SG = S22_Inv %*% S21 %*% Coef_FG %*% solve(diag(sqrt(megval2)))
+      colnames(Coef_SG) <- paste("V", 1:ncol(Coef_SG),
+                                 sep = "")
+      M3 <- eigen(diag(diag(S11)))
+      megval3 <- M3$values
+      megvec3 <- M3$vectors
+      D11_12 = megvec3 %*% diag(1/sqrt(megval3)) %*% t(megvec3)
+      M4 <- eigen(diag(diag(S22)))
+      megval4 <- M4$values
+      megvec4 <- M4$vectors
+      D22_12 = megvec4 %*% diag(1/sqrt(megval4)) %*% t(megvec4)
+      Rux <- t(t(Coef_FG) %*% S11 %*% D11_12)
+      rownames(Rux) <- colnames(FGV)
+      Rvy <- t(t(Coef_SG) %*% S22 %*% D22_12)
+      rownames(Rvy) <- colnames(SGV)
+
+      if (center == TRUE) {
+        FG_A = scale(FGV, center = TRUE, scale = FALSE)
+        SG_A = scale(SGV, center = TRUE, scale = FALSE)
+      } else {
+        FG_A = FGV
+        SG_A = SGV
+      }
+      FG_A[is.na(FG_A)] = 0
+      SG_A[is.na(SG_A)] = 0
+      FG_SC = FG_A %*% Coef_FG
+      SG_SC = SG_A %*% Coef_SG
+      if (test == "Bartlett") {
+        n <- nrow(FGV)
+        p <- ncol(FGV)
+        q <- ncol(SGV)
+        QtdF <- length(coruv)
+        Bartlett <- as.data.frame(matrix(NA, QtdF, 5))
+        colnames(Bartlett) <- c("Canonical_pairs", "Lambda_Wilks",
+                                "Chi_square", "DF", "p_value")
+        Bartlett[, 1] <- paste("U", 1:QtdF, "V", 1:QtdF,
+                               sep = "")
+        i = 1
+        for (i in 1:QtdF) {
+          Lambda <- prod(1 - coruv[i:QtdF]^2)
+          chisq <- -((n - 1) - (p + q + 1)/2) * log(Lambda)
+          gl <- (p - i + 1) * (q - i + 1)
+          pValor <- pchisq(chisq, gl, ncp = 0, lower.tail = F)
+          Bartlett[i, 2] <- round(Lambda, 5)
+          Bartlett[i, 3] <- round(chisq, 5)
+          Bartlett[i, 4] <- gl
+          Bartlett[i, 5] <- round(pValor, 5)
+        }
+        teste <- Bartlett
+      }
+      if (test == "Rao") {
+        n <- nrow(FGV)
+        p1 <- ncol(FGV)
+        q1 <- ncol(SGV)
+        QtdF <- length(coruv)
+        Rao <- as.data.frame(matrix(NA, QtdF, 6))
+        colnames(Rao) <- c("Canonical pairs", "Lambda_Wilks",
+                           "F_value", "DF1", "DF2", "p_value")
+        Rao[, 1] <- paste("U", 1:QtdF, "V", 1:QtdF, sep = "")
+        for (i in 1:QtdF) {
+          p <- p1 - i + 1
+          q <- q1 - i + 1
+          t <- (n - 1) - (p + q + 1)/2
+          s <- ifelse((p^2 + q^2) <= 5, 1, sqrt((p^2 *
+                                                   q^2 - 4)/(p^2 + q^2 - 5)))
+          Lambda <- prod(1 - coruv[i:QtdF]^2)
+          gl1 <- p * q
+          gl2 <- (1 + t * s - p * q/2)
+          FVAL <- ((1 - Lambda^(1/s))/Lambda^(1/s)) *
+            gl2/gl1
+          pValor <- pf(FVAL, gl1, gl2, ncp = 0, lower.tail = FALSE)
+          Rao[i, 2] <- round(Lambda, 5)
+          Rao[i, 3] <- round(FVAL, 5)
+          Rao[i, 4] <- gl1
+          Rao[i, 5] <- round(gl2, 5)
+          Rao[i, 6] <- round(pValor, 5)
+        }
+        teste <- Rao
+      }
+      results = data.frame(cbind(cbind(varuv, coruv), teste[-1]))
+      if (collinearity == TRUE) {
+        colin = list(FGc = colindiag(FGV, verbose = FALSE),
+                     SGc = colindiag(SGV, verbose = FALSE))
+      } else {
+        colin = NULL
+      }
+      if (verbose == TRUE) {
+        cat("\n\n\nLevel", nam, "\n")
+        cat("---------------------------------------------------------------------------\n")
+        cat("Matrix (correlation/covariance) between variables of first group (FG)\n")
+        cat("---------------------------------------------------------------------------\n")
+        print(S11)
+        if (collinearity == TRUE) {
+          cat("---------------------------------------------------------------------------\n")
+          cat("Collinearity within first group \n")
+          cat("---------------------------------------------------------------------------\n")
+          colindiag(FGV)
+        }
+        cat("---------------------------------------------------------------------------\n")
+        cat("Matrix (correlation/covariance) between variables of second group (SG)\n")
+        cat("---------------------------------------------------------------------------\n")
+        print(S22)
+        if (collinearity == TRUE) {
+          cat("---------------------------------------------------------------------------\n")
+          cat("Collinearity within second group \n")
+          cat("---------------------------------------------------------------------------\n")
+          colindiag(SGV)
+        }
+        cat("---------------------------------------------------------------------------\n")
+        cat("Matrix (correlation/covariance) between FG and SG\n")
+        cat("---------------------------------------------------------------------------\n")
+        print(S12)
+        cat("---------------------------------------------------------------------------\n")
+        cat("Correlation of the canonical pairs and hipotesis testing \n")
+        cat("---------------------------------------------------------------------------\n")
+        print(results)
+        cat("---------------------------------------------------------------------------\n")
+        cat("Canonical coefficients of the first group \n")
+        cat("---------------------------------------------------------------------------\n")
+        print(Coef_FG)
+        cat("---------------------------------------------------------------------------\n")
+        cat("Canonical coefficients of the second group \n")
+        cat("---------------------------------------------------------------------------\n")
+        print(Coef_SG)
+        cat("---------------------------------------------------------------------------\n")
+        cat("Canonical loads of the first group \n")
+        cat("---------------------------------------------------------------------------\n")
+        print(Rux)
+        cat("---------------------------------------------------------------------------\n")
+        cat("Canonical loads of the second group \n")
+        cat("---------------------------------------------------------------------------\n")
+        print(Rvy)
+      }
+      tmp = structure(list(Matrix = MC, MFG = S11, MSG = S22,
+                           MFG_SG = S12, Coef_FG = Coef_FG, Coef_SG = Coef_SG,
+                           Loads_FG = Rux, Loads_SG = Rvy, Score_FG = FG_SC,
+                           Score_SG = SG_SC, Sigtest = results, collinearity = colin),
+                      class = "can_cor")
+      dfs[[paste(nam)]] = tmp
+    }
+    return(structure(dfs, class = "group_can_cor"))
+  }
+
+  if (!missing(.data)) {
+    FG = as.data.frame(dplyr::select(.data, !!!dplyr::quos(!!dplyr::enquo(FG))))
+    SG = as.data.frame(dplyr::select(.data, !!!dplyr::quos(!!dplyr::enquo(SG))))
   }
 
   if (nrow(FG) != nrow(SG)) {
@@ -61,14 +265,14 @@ can_corr = function(.data = NULL, FG = NULL, SG = NULL, use = "cor",
   mtr = megval2
   varuv <- as.data.frame(matrix(NA, length(mtr), 3))
   rownames(varuv) <- paste("U", 1:length(mtr), "V", 1:length(mtr),
-    sep = "")
+                           sep = "")
   colnames(varuv) <- c("Variance", "Proportion", "Cum_proportion")
   varuv[, "Variance"] <- mtr
   varuv[, "Proportion"] <- (mtr/sum(mtr)) * 100
   varuv[, "Cum_proportion"] <- cumsum(varuv[, "Proportion"])
   coruv <- as.matrix(sqrt(mtr), ncol = length(coruv), nrow = 1)
   rownames(coruv) <- paste("U", 1:length(coruv), "V", 1:length(coruv),
-    sep = "")
+                           sep = "")
   colnames(coruv) <- c("Correlation")
   Coef_FG = S11_12 %*% megvec2
   rownames(Coef_FG) <- colnames(FG)
@@ -106,7 +310,7 @@ can_corr = function(.data = NULL, FG = NULL, SG = NULL, use = "cor",
     QtdF <- length(coruv)
     Bartlett <- as.data.frame(matrix(NA, QtdF, 5))
     colnames(Bartlett) <- c("Canonical_pairs", "Lambda_Wilks",
-      "Chi_square", "DF", "p_value")
+                            "Chi_square", "DF", "p_value")
     Bartlett[, 1] <- paste("U", 1:QtdF, "V", 1:QtdF, sep = "")
     i = 1
     for (i in 1:QtdF) {
@@ -128,19 +332,18 @@ can_corr = function(.data = NULL, FG = NULL, SG = NULL, use = "cor",
     QtdF <- length(coruv)
     Rao <- as.data.frame(matrix(NA, QtdF, 6))
     colnames(Rao) <- c("Canonical pairs", "Lambda_Wilks",
-      "F_value", "DF1", "DF2", "p_value")
+                       "F_value", "DF1", "DF2", "p_value")
     Rao[, 1] <- paste("U", 1:QtdF, "V", 1:QtdF, sep = "")
     for (i in 1:QtdF) {
       p <- p1 - i + 1
       q <- q1 - i + 1
       t <- (n - 1) - (p + q + 1)/2
       s <- ifelse((p^2 + q^2) <= 5, 1, sqrt((p^2 * q^2 -
-        4)/(p^2 + q^2 - 5)))
+                                               4)/(p^2 + q^2 - 5)))
       Lambda <- prod(1 - coruv[i:QtdF]^2)
       gl1 <- p * q
       gl2 <- (1 + t * s - p * q/2)
-      FVAL <- ((1 - Lambda^(1/s))/Lambda^(1/s)) *
-        gl2/gl1
+      FVAL <- ((1 - Lambda^(1/s))/Lambda^(1/s)) * gl2/gl1
       pValor <- pf(FVAL, gl1, gl2, ncp = 0, lower.tail = FALSE)
       Rao[i, 2] <- round(Lambda, 5)
       Rao[i, 3] <- round(FVAL, 5)
@@ -151,60 +354,61 @@ can_corr = function(.data = NULL, FG = NULL, SG = NULL, use = "cor",
     teste <- Rao
   }
   results = data.frame(cbind(cbind(varuv, coruv), teste[-1]))
-if (collinearity == TRUE){
-  colin = list(FG = colindiag(FG, verbose = FALSE),
-               SG = colindiag(SG, verbose = FALSE))
-} else {
-  colin = NULL
-}
-if (verbose == TRUE){
-  cat("---------------------------------------------------------------------------\n")
-  cat("Matrix (correlation/covariance) between variables of first group (FG)\n")
-  cat("---------------------------------------------------------------------------\n")
-  print(S11)
-    if (collinearity == TRUE){
-  cat("\n---------------------------------------------------------------------------\n")
-  cat("Collinearity within first group \n")
-  cat("---------------------------------------------------------------------------\n")
-  colindiag(FG)
-    }
-  cat("\n---------------------------------------------------------------------------\n")
-  cat("Matrix (correlation/covariance) between variables of second group (SG)\n")
-  cat("---------------------------------------------------------------------------\n")
-  print(S22)
-  if (collinearity == TRUE){
-  cat("\n---------------------------------------------------------------------------\n")
-  cat("Collinearity within second group \n")
-  cat("---------------------------------------------------------------------------\n")
-  colindiag(SG)
+  if (collinearity == TRUE) {
+    colin = list(FG = colindiag(FG, verbose = FALSE), SG = colindiag(SG,
+                                                                     verbose = FALSE))
+  } else {
+    colin = NULL
   }
-  cat("\n---------------------------------------------------------------------------\n")
-  cat("Matrix (correlation/covariance) between FG and SG)\n")
-  cat("---------------------------------------------------------------------------\n")
-  print(S12)
-  cat("\n---------------------------------------------------------------------------\n")
-  cat("Correlation of the canonical pairs and hipotesis testing \n")
-  cat("---------------------------------------------------------------------------\n")
-  print(results)
-  cat("\n---------------------------------------------------------------------------\n")
-  cat("Canonical coefficients of the first group \n")
-  cat("---------------------------------------------------------------------------\n")
-  print(Coef_FG)
-  cat("\n---------------------------------------------------------------------------\n")
-  cat("Canonical coefficients of the second group \n")
-  cat("---------------------------------------------------------------------------\n")
-  print(Coef_SG)
-  cat("\n---------------------------------------------------------------------------\n")
-  cat("Canonical loads of the first group \n")
-  cat("---------------------------------------------------------------------------\n")
-  print(Rux)
-  cat("\n---------------------------------------------------------------------------\n")
-  cat("Canonical loads of the second group \n")
-  cat("---------------------------------------------------------------------------\n")
-  print(Rvy)
-}
+  if (verbose == TRUE) {
+    cat("---------------------------------------------------------------------------\n")
+    cat("Matrix (correlation/covariance) between variables of first group (FG)\n")
+    cat("---------------------------------------------------------------------------\n")
+    print(S11)
+    if (collinearity == TRUE) {
+      cat("---------------------------------------------------------------------------\n")
+      cat("Collinearity within first group \n")
+      cat("---------------------------------------------------------------------------\n")
+      colindiag(FG)
+    }
+    cat("---------------------------------------------------------------------------\n")
+    cat("Matrix (correlation/covariance) between variables of second group (SG)\n")
+    cat("---------------------------------------------------------------------------\n")
+    print(S22)
+    if (collinearity == TRUE) {
+      cat("---------------------------------------------------------------------------\n")
+      cat("Collinearity within second group \n")
+      cat("---------------------------------------------------------------------------\n")
+      colindiag(SG)
+    }
+    cat("---------------------------------------------------------------------------\n")
+    cat("Matrix (correlation/covariance) between FG and SG\n")
+    cat("---------------------------------------------------------------------------\n")
+    print(S12)
+    cat("---------------------------------------------------------------------------\n")
+    cat("Correlation of the canonical pairs and hipotesis testing \n")
+    cat("---------------------------------------------------------------------------\n")
+    print(results)
+    cat("---------------------------------------------------------------------------\n")
+    cat("Canonical coefficients of the first group \n")
+    cat("---------------------------------------------------------------------------\n")
+    print(Coef_FG)
+    cat("---------------------------------------------------------------------------\n")
+    cat("Canonical coefficients of the second group \n")
+    cat("---------------------------------------------------------------------------\n")
+    print(Coef_SG)
+    cat("---------------------------------------------------------------------------\n")
+    cat("Canonical loads of the first group \n")
+    cat("---------------------------------------------------------------------------\n")
+    print(Rux)
+    cat("---------------------------------------------------------------------------\n")
+    cat("Canonical loads of the second group \n")
+    cat("---------------------------------------------------------------------------\n")
+    print(Rvy)
+  }
   invisible(structure(list(Matrix = MC, MFG = S11, MSG = S22,
-    MFG_SG = S12, Coef_FG = Coef_FG, Coef_SG = Coef_SG,
-    Loads_FG = Rux, Loads_SG = Rvy, Score_FG = FG_SC, Score_SG = SG_SC,
-    Sigtest = results, collinearity = colin), class = "can_cor"))
+                           MFG_SG = S12, Coef_FG = Coef_FG, Coef_SG = Coef_SG, Loads_FG = Rux,
+                           Loads_SG = Rvy, Score_FG = FG_SC, Score_SG = SG_SC, Sigtest = results,
+                           collinearity = colin), class = "can_cor"))
+
 }
