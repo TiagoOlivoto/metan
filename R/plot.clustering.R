e73253dbed2db25d92478a0c650dcebec70600fe
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-05-09 16:33:54 -0300

    Plot an object of class clustering

--- R/plot.clustering.R
+++ R/plot.clustering.R
@@ -1,3 +1,16 @@
+#' Plot an object of class clustering
+#'
+#' Plot the multitrait stability index using ggplot-generated graphics. .
+#'
+#'
+#' @param x An object of class \code{clustering}
+#' @param type The type of plot. Must be one of the 'dendrogram' or
+#' 'cophenetic'.
+#' @param ... Other arguments passed from the function \code{plot.dendrogram}.
+#' @method plot clustering
+#' @export
+#' @author Tiago Olivoto \email{tiagoolivoto@@gmail.com}
+#'
 plot.clustering = function(x, type = "dendrogram", ...){
   if (type == "dendrogram"){
     plot(as.dendrogram(x$hc), ...)
