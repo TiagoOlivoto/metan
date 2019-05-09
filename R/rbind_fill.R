c15e4f4c6cd458f654a061115950a70a1ea41fdb
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-05-09 16:42:38 -0300

    Combining data.frames by row, filling missing values

--- R/rbind_fill.R
+++ R/rbind_fill.R
@@ -1,4 +1,21 @@
-rbind_fill <- function(..., fill = "-") {
+#' Combining data.frames by row, filling missing values
+#'
+#' Helper function similar to \code{plyr::rbind.fill} that combines data.frames
+#' by row and fills with \code{.} missing values
+#'
+#'
+#' @param ... Input dataframes.
+#' @param fill What use to fill? Default is \code{"."}
+#' @author Tiago Olivoto \email{tiagoolivoto@@gmail.com}
+#' @export
+#' @examples
+#'
+#' df1 = data.frame(v1 = c(1, 2), v2 = c(2, 3))
+#' df2 = data.frame(v3 = c(4, 5))
+#' rbind_fill(df1, df2)
+#' rbind_fill(df1, df2, fill = "NA")
+#'
+rbind_fill <- function(..., fill = ".") {
     df <- list(...)
     ns <- unique(unlist(sapply(df, names)))
     do.call(rbind, lapply(df, function(x) {
