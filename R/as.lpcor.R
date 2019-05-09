0fbfef7b30ec67204e182cc3a79cec6ccdf384f1
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-05-09 16:10:58 -0300

    Coerce to an object of class lpcor

--- R/as.lpcor.R
+++ R/as.lpcor.R
@@ -1,3 +1,24 @@
+#' Coerce to an object of class lpcor
+#'
+#' Functions to check if an object is of class \code{lpcor}, or coerce it if
+#' possible.
+#'
+#'
+#' @param ... A comma-separated list of matrices to be coerced to a list.
+#' @importFrom methods as
+#' @export
+#' @examples
+#'
+#' library(METAAB)
+#' library(dplyr)
+#' mt_num = mtcars %>% select_if(., is.numeric)
+#' lpdata = as.lpcor(cor(mt_num[1:5]),
+#'                   cor(mt_num[1:5]),
+#'                   cor(mt_num[2:6]),
+#'                   cor(mt_num[4:8]))
+#' is.lpcor(lpdata)
+#'
+#'
 as.lpcor = function(...){
   data = list(...)
   if(length(which(sapply(data, function(x) identical(nrow(x), ncol(x)))==TRUE)) != length(data)){
@@ -10,6 +31,3 @@ as.lpcor = function(...){
   names(data) = paste("mat", 1:length(data), sep = "_")
   invisible(structure(data, class = "lpcor"))
 }
-
-
-
