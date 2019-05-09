dd8606a771a610acc8ba1ab9a472267359b76afc
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-05-09 16:10:04 -0300

    Coerce to an object of class group_factors

--- R/as.group_factors.R
+++ R/as.group_factors.R
@@ -1,3 +1,29 @@
+#' Coerce to an object of class group_factors
+#'
+#' Functions to check if an object is of class \code{group_factors}, or coerce
+#' it if possible.
+#'
+#' A dataframe may be easily coerced to be split into named subsets based on
+#' each combination of factors existing in the original dataframe. For example,
+#' if the original data has two columns, namely ENV (four levels) and HIB (ten
+#' levels), and ten numeric columns, then using \code{as.group_factors} will
+#' split the data into 40 10-columns subsets, corresponding to each combination
+#' of ENV x HIB.
+#'
+#' @param x The input data.
+#' @param verbose Logical argument. If \code{verbose = FALSE} the code will run
+#' silently.
+#' @importFrom methods as
+#' @export
+#' @examples
+#'
+#' library(METAAB)
+#' spdata = as.group_factors(iris)
+#'
+#' spdata2 = as.group_factors(CO2)
+#'
+#' is.group_factors(spdata2)
+#'
 as.group_factors = function(x, verbose = TRUE) {
   grouped <- group_by_if(x, is.factor)
   names <- rlang::eval_bare(rlang::expr(paste(!!!group_keys(grouped), sep = " / ")))
