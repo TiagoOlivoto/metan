b553adbe3c43955e5660c5c86cc1d4174f41616a
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-05-09 16:29:19 -0300

    Make a symetric matrix based on an upper or lower triangular matrix

--- R/make_sym.R
+++ R/make_sym.R
@@ -1,3 +1,22 @@
+#' Make a symetric matrix based on an upper or lower triangular matrix
+#'
+#' This function help users to easily make a symmetric matrix using a lower or
+#' an upper triangular matrix.
+#'
+#'
+#' @param .matrix The upper or lower triangular matrix.
+#' @param make The triangular to built. Default is \code{"upper"}. In this
+#' case, a symetric matrix will be built based on the values of a lower
+#' triangular values.
+#' @author Tiago Olivoto \email{tiagoolivoto@@gmail.com}
+#' @export
+#' @examples
+#'
+#' m <- matrix(NA,4,4)
+#' m[lower.tri(m)] <- 1:16
+#' m
+#' make_sym(m)
+#'
 make_sym <- function(.matrix, make = "upper") {
   if(make == "upper"){
     .matrix[upper.tri(.matrix)] <- t(.matrix)[upper.tri(.matrix)]
