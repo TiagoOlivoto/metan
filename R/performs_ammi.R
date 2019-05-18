25ba1cbd364f5f62a1e6d8147858d9186f413657
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-05-18 02:03:36 -0300

    Addtive main effect and multiplicative interaction

--- R/performs_ammi.R
+++ R/performs_ammi.R
@@ -1,16 +1,17 @@
-#' Performs Additive Main effects and Multiplicative Interaction
+#' Additive Main effects and Multiplicative Interaction
 #'
 #' Compute the Additive Main effects and Multiplicative interaction. This is a
 #' helper function for other procedures performed in the WAASB package such as
-#' \code{\link{WAAS.AMMI}}, \code{\link{cv_ammi}}, and \code{\link{cv_ammif}}.
+#' \code{\link{waas}}, \code{\link{cv_ammi}}, and \code{\link{cv_ammif}}.
 #'
-#'
-#' @param ENV The name of the collum that contains the levels of the
+#' @param .data The dataset containing the columns related to Environments,
+#' Genotypes, replication/block and response variable(s).
+#' @param env The name of the collum that contains the levels of the
 #' environments
-#' @param GEN The name of the collum that contains the levels of the genotypes
-#' @param REP The name of the collum that contains the levels of the
+#' @param gen The name of the collum that contains the levels of the genotypes
+#' @param rep The name of the collum that contains the levels of the
 #' replications/blocks
-#' @param Y The response variable
+#' @param resp The response variable
 #' @return
 #'
 #' \item{ANOVA}{The analysis of variance for the AMMI model.}
@@ -26,24 +27,30 @@
 #' @examples
 #'
 #' library(metan)
-#' ammi_model = with(data_ge, performs_ammi(ENV, GEN, REP, GY))
+#' ammi_model = performs_ammi(data_ge, ENV, GEN, REP, GY)
 #'
 #'
-performs_ammi <- function(ENV, GEN, REP, Y) {
-    resp <- paste(deparse(substitute(Y)))
-    ENV <- as.factor(ENV)
-    GEN <- as.factor(GEN)
-    REP <- as.factor(REP)
+performs_ammi <- function(.data, env, gen, rep, resp) {
+    data = .data %>% select(!!enquo(env), !!enquo(gen), !!enquo(rep), !!enquo(resp)) %>%
+        as.data.frame()
+    names(data) = c("ENV", "GEN", "REP", "Y")
+    GEN <- factor(data[,2])
+    ENV <- factor(data[,1])
+    REP <- factor(data[,3])
+    Y <- eval(data[,4])
     nenv <- length(unique(ENV))
     ngen <- length(unique(GEN))
-    minimo <- min(ngen, nenv)
+    minimo <- min(ngen, nenv) - 1
     nrep <- length(unique(REP))
     model <- aov(Y ~ ENV + REP %in% ENV + GEN + ENV:GEN)
+    df <- fortify(model)
     datares <- model$model
     datares$factors <- paste(datares$ENV, datares$GEN)
-    df <- fortify(model)
-    df <- data.frame(fitted = df[, 8], resid = df[, 9], stdres = df[, 10])
-    residuals <- cbind(datares, df)
+    residuals = cbind(datares, df %>% select(fitted = .fitted, resid = .resid, stdres = .stdresid))
+    if (minimo <= 2) {
+        cat("\nWarning. The analysis AMMI is not possible.")
+        cat("\nThe number of environments and number of genotypes must be greater than 2\n")
+    }
     mm <- anova(model)
     nn <- mm[2, ]
     mm[2, ] <- mm[3, ]
@@ -56,58 +63,23 @@ performs_ammi <- function(ENV, GEN, REP, Y) {
     probint <- anova[4, 5]
     DFE <- df.residual(model)
     MSE <- deviance(model)/DFE
-    raw <- data.frame(ENV, GEN, Y)
-    MEANS <- tapply(raw[, 3], raw[, c(1, 2)], mean)
-    xx <- rownames(MEANS)
-    yy <- colnames(MEANS)
-    fila <- length(xx)
-    col <- length(yy)
-    total <- fila * col
-    x <- character(length = total)
-    y <- character(length = total)
-    z <- numeric(length = total)
-    k <- 0
-    for (i in 1:fila) {
-        for (j in 1:col) {
-            k <- k + 1
-            x[k] <- xx[i]
-            y[k] <- yy[j]
-            z[k] <- MEANS[i, j]
-        }
-    }
-    MEANS <- data.frame(ENV = x, GEN = y, Y = z)
-    x <- MEANS[, 1]
-    y <- MEANS[, 2]
-    z <- MEANS[, 3]
-    model2 <- lm(z ~ x + y)
-    for (i in 1:length(z)) {
-        if (is.na(z[i]))
-            z[i] <- predict(model2, data.frame(x = MEANS[i, 1], y = MEANS[i, 2]))
-    }
-    MEANS <- data.frame(ENV = x, GEN = y, Y = z)
-    model1 <- lm(Y ~ ENV + GEN, data = MEANS)
-    residual <- model1$residuals
-    MEANS <- data.frame(MEANS, RESIDUAL = residual)
-    mlabel <- names(MEANS)
-    names(MEANS) <- c(mlabel[1:2], resp, mlabel[4])
-    OUTRES <- MEANS[order(MEANS[, 1], MEANS[, 2]), ]
-    OUTRES2 <- by(OUTRES[, 4], OUTRES[, c(2, 1)], function(x) sum(x, na.rm = TRUE))
-    OUTMED <- by(OUTRES[, 3], OUTRES[, c(2, 1)], function(x) sum(x, na.rm = TRUE))
-    s <- svd(OUTRES2)
-    U <- s$u
-    L <- s$d
-    V <- s$v
-    L <- L[1:minimo]
-    SS <- (L^2) * nrep
+    MEANS <- data %>% group_by(ENV, GEN) %>% summarise(Y = mean(Y)) %>% ungroup()
+    residual <- residuals(lm(Y ~ ENV + GEN, data = MEANS))
+    MEANS %<>% mutate(RESIDUAL = residual)
+    s <- svd(t(matrix(residual, nenv, byrow = T)))
+    U <- s$u[, 1:minimo]
+    LL <- diag(s$d[1:minimo])
+    V <- s$v[, 1:minimo]
+    SS <- (s$d[1:minimo]^2) * nrep
     SUMA <- sum(SS)
     percent <- (1/SUMA) * SS * 100
-    DFAMMI <- rep(0, minimo)
+    DFAMMI <- replicate(minimo, 0)
     acum <- DFAMMI
     MSAMMI <- DFAMMI
     F.AMMI <- DFAMMI
     PROBF <- DFAMMI
     acumula <- 0
-    for (i in 1:(minimo - 1)) {
+    for (i in 1:(minimo)) {
         DF <- (ngen - 1) + (nenv - 1) - (2 * i - 1)
         if (DF <= 0)
             break
@@ -130,42 +102,36 @@ performs_ammi <- function(ENV, GEN, REP, Y) {
     SSAMMI <- SSAMMI[SSAMMI$Df > 0, ]
     nss <- nrow(SSAMMI)
     row.names(SSAMMI) <- paste("PC", 1:nss, sep = "")
-    LL <- sqrt(diag(L))
-    SCOREG <- U %*% LL
-    SCOREE <- V %*% LL
-    SCORES <- rbind(SCOREG, SCOREE)
-    colnames(SCORES) <- paste("PC", 1:nssammi, sep = "")
-    MSCORES <- SCORES[1:ngen, ]
-    NSCORES <- SCORES[(ngen + 1):(ngen + nenv), ]
-    MGEN <- data.frame(type = "GEN", Y = apply(OUTMED, 1, mean), MSCORES)
-    MENV <- data.frame(type = "ENV", Y = apply(OUTMED, 2, mean), NSCORES)
-    bplot <- rbind(MGEN, MENV)
-    bplot <- bplot[, 1:(nss + 2)]
-    mlabel <- names(bplot)
-    names(bplot) <- c(mlabel[1], resp, mlabel[c(-1, -2)])
-    if (minimo <= 2) {
-        cat("\nWarning. The analysis AMMI is not possible.")
-        cat("\nThe number of environments and number of genotypes must be greater than 2\n")
-    }
+    SCOREG <- U %*% LL^0.5
+    SCOREE <- V %*% LL^0.5
+    colnames(SCOREG) <- colnames(SCOREE) <- paste("PC", 1:minimo, sep = "")
+    bplot = MEANS %>%
+        group_by(GEN) %>%
+        summarise(Y = mean(Y)) %>%
+        mutate(type = "GEN") %>%
+        rename(Code = GEN) %>%
+        cbind(., SCOREG) %>%
+        rbind(.,
+              MEANS %>%
+        group_by(ENV) %>%
+        summarise(Y = mean(Y)) %>%
+        mutate(type = "ENV") %>%
+        rename(Code = ENV) %>%
+        cbind(., SCOREE)) %>%
+        select(type, Code, everything())
     PC <- SSAMMI %>% dplyr::select(-percent, -acum, everything())
-    anova <- mm
-    anova <- cbind(Percent = ".", anova)
-    anova <- anova %>% dplyr::select(-Percent, everything())
-    anova <- cbind(Accumul = ".", anova)
-    anova <- anova %>% dplyr::select(-Accumul, everything())
+    resid <- as.data.frame(anova[nrow(anova), ])
+    rownames(resid) = "Residuals"
     sum <- as.data.frame(anova[nrow(anova), ])
     sum$Df <- sum(anova$Df)
     sum$`Sum Sq` <- sum(anova$`Sum Sq`)
     sum$`Mean Sq` <- sum$`Sum Sq`/sum$Df
     rownames(sum) <- "Total"
-    ERRO <- anova[nrow(anova), ]
-    ERRO <- rbind(ERRO, sum)
-    anova <- anova[-nrow(anova), -c(6, 7)]
+    ERRO <- rbind(resid, sum)
     names(PC) <- paste(c("Df", "Sum Sq", "Mean Sq", "F value", "Pr(>F)", "Percent",
         "Accumul"))
-    anova <- rbind_fill(anova, PC, ERRO)
-    object <- list(ANOVA = anova, analysis = PC, means = MEANS, biplot = bplot, residuals = residuals,
-        probint = probint)
-    class(object) <- "WAASB"
-    return(object)
+    anova <- rbind_fill(mm[-nrow(mm),], PC, ERRO)
+    invisible(structure(list(ANOVA = anova, analysis = PC, means = MEANS, biplot = bplot, residuals = residuals,
+        probint = probint),
+        class = "WAASB"))
 }
