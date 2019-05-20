e18afcd39c3f69f7e527afd0c63920f998dc39f6
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-05-20 02:06:42 -0300

    Eberhart and Russell's regression model

--- R/ge_reg.R
+++ R/ge_reg.R
@@ -114,7 +114,7 @@ ge_reg = function(.data,
                            levels(data$GEN), "Pooled error")
     temp = structure(list(data = iamb2,
                 anova = as_tibble(rownames_to_column(anovadf, "SV")),
-                regression = as_tibble(GEN = levels(mydf$GEN),
+                regression = data.frame(GEN = levels(mydf$GEN),
                                         Mean = apply(matx, 1, mean),
                                         bij = bij,
                                         sdij = S2di,
