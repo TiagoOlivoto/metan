b563bc7dd0543c2d677b98898c65469f65a80057
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-05-22 15:32:44 -0300

    Vignettes

---
+++ inst/doc/vignettes_metan.R
@@ -0,0 +1,76 @@
+## ---- message=FALSE, warning=FALSE---------------------------------------
+library(metan)
+library(cowplot) # used to arrange the graphics
+library(kableExtra) # Used to make the tables
+library(dplyr) # for data manipulation and pipe %>%
+library(tibble) # for manipulating rownmanes
+# Function to make HTML tables
+print_table = function(table){
+  kable(table, "html", digits = 3) %>%
+    kable_styling(bootstrap_options = c("striped", "hover", "condensed", "responsive"), font_size = 12)
+}
+str(data_ge)
+
+## ---- fig.height=3.5, fig.width=5----------------------------------------
+CVAL = cv_ammif(data_ge,
+                resp = GY,
+                gen = GEN,
+                env = ENV,
+                rep = REP,
+                nboot = 5,
+                nrepval = 2)
+plot(CVAL)
+
+## ------------------------------------------------------------------------
+model <- data_ge %>% waas(ENV, GEN, REP, GY)
+
+## ---- fig.height=8, fig.width=5,  message=FALSE, warning=FALSE-----------
+p1 <- plot_scores(model$GY, axis.expand = 1.2)
+p2 <- plot_scores(model$GY,
+                  type = 1,
+                  polygon = TRUE,
+                  col.gen = "black",
+                  col.env = "gray70",
+                  col.segm.env = "gray70",
+                  axis.expand = 1.5)
+plot_grid(p1, p2,
+          align = "v",
+          labels = c("p1","p2"),
+          ncol = 1)
+
+## ------------------------------------------------------------------------
+predicted <- predict(model, naxis = 4)
+print_table(head(predicted$GY))
+
+## ------------------------------------------------------------------------
+model2 <- data_ge %>% waasb(ENV, GEN, REP, GY)
+
+## ------------------------------------------------------------------------
+print_table(model2$GY$LRT)
+
+print_table(model2$GY$random)
+
+print_table(model2$GY$ESTIMATES)
+
+print_table(model2$GY$blupGEN)
+
+## ---- fig.height=8, fig.width=5------------------------------------------
+p1 <- plot_blup(model2$GY)
+p2 <- plot_blup(model2$GY,
+                prob = 0.1,
+                col.shape  =  c("gray20", "gray80")) + coord_flip()
+plot_grid(p1, p2,
+          align = "v",
+          labels = c("p1", "p2"),
+          ncol = 1)
+
+## ------------------------------------------------------------------------
+print_table(head(model2$GY$BLUPgge))
+
+## ------------------------------------------------------------------------
+stat_ge <- data_ge %>% ge_stats(ENV, GEN, REP, GY)
+dat = stat_ge$GY
+
+## ---- eval=FALSE---------------------------------------------------------
+#  summary(stat_ge, export = TRUE)
+
