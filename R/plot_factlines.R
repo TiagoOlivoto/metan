3ee73a17a8eecdf8c2178bb785ec25c41963e5b6
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-05-27 21:43:32 -0300

    Faste way to create line plot

---
+++ R/plot_factlines.R
@@ -0,0 +1,171 @@
+#' @title Fast way to create a line plot
+#' @description Create a graphic to show a fitted line based on numerical 
+#' variables and one grouping variable.
+#' @param data The data set
+#' @param x The variable in data to be shown in the x axis
+#' @param y The variable in data to be shown in the y axis
+#' @param group The grouping variable
+#' @param fit The polinomial degree to use. It must be an integer
+#' between 1 (linear fit) to 4 (fourth-order polynomial regression.),
+#' or a numeric vector with the same length of the variable in \code{group}
+#' @param level The fonfidence level
+#' @param xlab The x lab
+#' @param ylab The y lab
+#' @param col The colour to be used in the line plot and points
+#' @param alpha The alpha for the color in confidence band
+#' @param size.shape The size for the shape in plot
+#' @param size.line The size for the line in the plot
+#' @param cex The size of the text
+#' @param fontfam The family of the font text
+#' @export
+#' @seealso \code{\link{plot_line}, \code{\link{plot_factbars}}
+
+plot_factlines = function(.data,
+                          x,
+                          y,
+                          group,
+                          fit,
+                          level = 0.95,
+                          xlab = NULL,
+                          ylab = NULL,
+                          legend.position = "bottom",
+                          grid = FALSE,
+                          col = TRUE,
+                          alpha = 0.2,
+                          size.shape = 1.5,
+                          size.line = 1,
+                          cex = 12,
+                          fontfam = "sans",
+                          theme = theme_waasb()){
+
+  if(length(fit)==1 & grid == TRUE){
+    stop("When grid is TRUE the argument fit must have the same length of the grouping variable.")
+  }
+  
+  if(max(fit)>=5){
+    stop("The maximum polynomial degree is 4.")
+  }
+  cl = match.call()
+  data2 = data %>% select(factors = !!enquo(group),
+                          x = !!enquo(x),
+                          y = !!enquo(y))
+
+group = as.factor(data2$factors)
+p_smooth = list()
+levels = levels(group)
+
+if (length(fit)>1){
+for (i in 1:length(levels)){
+
+  levelname = levels[i]
+  mycond <- quote(group == levelname) 
+
+  if(fit[i] == 1){
+    formula = as.formula("y ~ x")
+  }
+  if(fit[i] == 2){
+    formula = as.formula("y ~ poly(x, 2)")
+  }
+  if(fit[i] == 3){
+    formula = as.formula("y ~ poly(x, 3)")
+  }
+  if(fit[i] == 4){
+    formula = as.formula("y ~ poly(x, 4)")
+  }
+
+if (col == FALSE){
+  linetype = i
+  p_smooth[[paste(levels[i])]] = ggplot2::stat_smooth(method = "lm",
+                                             formula = formula,
+                                             data = subset(data2, eval(mycond)),
+                                             level = level,
+                                             linetype = linetype,
+                                             alpha = alpha,
+                                             col = "black",
+                                             size = size.line)
+} else{
+linetype = 1
+p_smooth[[paste(levels[i])]] = ggplot2::stat_smooth(method = "lm",
+                                            formula = formula,
+                                            data = subset(data2, eval(mycond)),
+                                            level = level,
+                                            linetype = linetype,
+                                            alpha = alpha,
+                                            size = size.line)
+}
+}
+} else{
+  if(fit == 1){
+    formula = as.formula("y ~ x")
+  }
+  if(fit == 2){
+    formula = as.formula("y ~ poly(x, 2)")
+  }
+  if(fit == 3){
+    formula = as.formula("y ~ poly(x, 3)")
+  }
+  if(fit == 4){
+    formula = as.formula("y ~ poly(x, 4)")
+  }
+  
+  if (col == TRUE){
+    p_smooth = ggplot2::stat_smooth(method = "lm",
+                                    formula = formula,
+                                    data = data2,
+                                    level = level,
+                                    linetype = 1,
+                                    size = size.line)
+  } else{
+    
+    p_smooth = ggplot2::stat_smooth(method = "lm",
+                                    formula = formula,
+                                    data = data2,
+                                    level = level,
+                                    linetype = 1,
+                                    col = "black",
+                                    size = size.line)    
+  }
+}
+if(grid == TRUE){
+  legend.position = "none"
+} else{legend.position = legend.position}
+
+if (is.null(ylab) == T){
+  ylab = cl$y
+}else {ylab = ylab}
+
+if (is.null(xlab) == T){
+  xlab = cl$x
+}else {xlab = xlab}
+
+if (col == FALSE){
+p = ggplot2::ggplot(data2, aes(x = x, y = y)) +
+    ggplot2::geom_point(aes(shape = factors), size = size.shape)
+} else{
+  if (length(fit)>1){
+p = ggplot2::ggplot(data2, aes(x = x, y = y, colour = factors)) + 
+    ggplot2::geom_point(aes(colour = factors), size = size.shape)
+  } else{
+    p = ggplot2::ggplot(data2, aes(x = x, y = y)) + 
+      ggplot2::geom_point(aes(colour = factors), size = size.shape)
+  }
+}
+p = p + p_smooth +
+  theme %+replace%
+  theme(axis.ticks.length = unit(.2, "cm"),
+        axis.text = element_text(size = cex, family = fontfam, colour = "black"),
+        axis.title = element_text(size = cex,  family = fontfam, colour = "black"),
+        axis.ticks = element_line(colour = "black"),
+        plot.margin = margin(0.2, 0.2, 0.2, 0.2, "cm"),
+        legend.title = element_blank(),
+        legend.position = legend.position,
+        legend.text = element_text(size = cex, family = fontfam))+
+ggplot2::labs(y = ylab, x = xlab)
+
+if (grid == TRUE){
+    p = p + ggplot2::facet_wrap(~factors, scales = "free")
+} else{p = p}
+
+return(p)
+
+}
---
+++ R/plot_lines.R
@@ -0,0 +1,107 @@
+#' @title Fast way to create a line plot
+#' @description Create a graphic with fitted line based on numerical 
+#' variables.
+#' @param data The data set
+#' @param x The variable in data to be shown in the x axis
+#' @param y The variable in data to be shown in the y axis
+#' @param fit The polinomial degree to use. It must be between
+#' 1 (linear fit) to 4 (fourth-order polynomial regression.)
+#' @param level The fonfidence level
+#' @param xlab The x lab
+#' @param ylab The y lab
+#' @param col The colour to be used in the line plot and points
+#' @param alpha The alpha for the color in confidence band
+#' @param size.shape The size for the shape in plot
+#' @param size.line The size for the line in the plot
+#' @param cex The size of the text
+#' @param fontfam The family of the font text
+#' @export
+#' @seealso \code{\link{plot_fatcurves}}
+plot_line = function(.data,
+                    x,
+                    y,
+                    fit,
+                    level = 0.95,
+                    xlab = NULL,
+                    ylab = NULL,
+                    col = "red",
+                    alpha = 0.2,
+                    size.shape = 1.5,
+                    size.line = 1,
+                    cex = 12,
+                    fontfam = "sans"){
+  
+  cl = match.call()
+if(col == TRUE){
+  stop(paste0("The argument col = ", cl$col, " is invalid. Please, informe one valid color or FALSE to a black and white plot"))
+}  
+  data2 = .data %>% select(x = !!enquo(x), y = !!enquo(y))
+  if(fit == 1){
+    formula = as.formula("y ~ x")
+  }
+  if(fit == 2){
+    formula = as.formula("y ~ poly(x, 2)")
+  }
+  if(fit == 3){
+    formula = as.formula("y ~ poly(x, 3)")
+  }
+  if(fit == 4){
+    formula = as.formula("y ~ poly(x, 4)")
+  }
+
+if (col == FALSE){
+  linetype = 1
+  p_smooth = ggplot2::stat_smooth(method = "lm",
+                                             formula = formula,
+                                             data = data2,
+                                             level = level,
+                                             alpha = alpha,
+                                             col = "black",
+                                             size = size.line)
+} else{
+linetype = 1
+p_smooth = ggplot2::stat_smooth(method = "lm",
+                                            formula = formula,
+                                            data = data2,
+                                            level = level,
+                                            alpha = alpha,
+                                            col = col,
+                                            size = size.line)
+}
+
+
+
+if (is.null(ylab) == T){
+  ylab = cl$y
+}else {ylab = ylab}
+
+if (is.null(xlab) == T){
+  xlab = cl$x
+}else {xlab = xlab}
+
+
+  if (col == FALSE){
+    p = ggplot2::ggplot(data2, aes(x = x, y = y)) +
+      ggplot2::geom_point(size = size.shape)
+  } else{
+    p = ggplot2::ggplot(data2, aes(x = x, y = y)) + 
+      ggplot2::geom_point(size = size.shape, col = col)
+  }
+
+p = p + p_smooth +
+  ggplot2::theme_bw()+
+  ggplot2::theme(axis.ticks.length = unit(.2, "cm"),
+        axis.text = element_text(size = cex, family = fontfam, colour = "black"),
+        axis.title = element_text(size = cex,  family = fontfam, colour = "black"),
+        axis.ticks = element_line(colour = "black"),
+        plot.margin = margin(0.2, 0.2, 0.2, 0.2, "cm"),
+        legend.title = element_blank(),
+        legend.text = element_text(size = cex, family = fontfam),
+        panel.border = element_rect(colour = "black", fill=NA, size=1),
+        panel.grid.major.x = element_blank(), panel.grid.major.y = element_blank(),
+        panel.grid.minor.x = element_blank(), panel.grid.minor.y = element_blank())+
+ggplot2::labs(y = ylab, x = xlab)
+
+return(p)
+
+}
