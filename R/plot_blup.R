9135be03052bc2e50dd3d243b7d269d4f3180dfc
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-06-06 03:13:44 -0300

    Plot the BLUPs for genotypes

--- R/plot_blup.R
+++ R/plot_blup.R
@@ -55,17 +55,17 @@
 #'
 #'
 plot_blup <- function(x, prob = 0.05, export = FALSE, file.type = "pdf", file.name = NULL,
-    theme = theme_waasb(), width = 6, height = 6, size.err.bar = 0.5, size.shape = 3.5,
-    size.tex.lab = 12, height.err.bar = 0.3, x.lim = NULL, x.breaks = waiver(), col.shape = c("blue",
-        "red"), y.lab = "Genotypes", x.lab = "Predicted Grain Yield", resolution = 300,
-    ...) {
- if(!class(x) ==  "waasb"){
-     stop("The object 'x' must be of class 'waasb'.")
- }
+                      theme = theme_waasb(), width = 6, height = 6, size.err.bar = 0.5, size.shape = 3.5,
+                      size.tex.lab = 12, height.err.bar = 0.3, x.lim = NULL, x.breaks = waiver(), col.shape = c("blue",
+                                                                                                                "red"), y.lab = "Genotypes", x.lab = "Predicted Grain Yield", resolution = 300,
+                      ...) {
+    if(!class(x) ==  "waasb"){
+        stop("The object 'x' must be of class 'waasb'.")
+    }
     PROB <- ((1 - (1 - prob))/2) + (1 - prob)
     t <- qt(PROB, 100)
-    GV <- as.numeric(substr(x$ESTIMATES[2, 2], start = 1, stop = 5))
-    AccuGen <- as.numeric(substr(x$ESTIMATES[8, 2], start = 1, stop = 5))
+    GV <- as.numeric(substr(data.frame(x$ESTIMATES)[2, 2], start = 1, stop = 6))
+    AccuGen <- as.numeric(substr(data.frame(x$ESTIMATES)[8, 2], start = 1, stop = 6))
     Limits <- t * sqrt(((1 - AccuGen) * GV))
     blup <- x$blupGEN
     blup <- dplyr::mutate(blup, LL = Predicted - Limits, UL = Predicted + Limits)
@@ -74,13 +74,15 @@ plot_blup <- function(x, prob = 0.05, export = FALSE, file.type = "pdf", file.na
     blup$Mean <- ifelse(blup$Predicted < mean(blup$Predicted), "below", "above")
     blup$CI <- blup$Predicted - blup$LL
     mean <- mean(blup$Predicted)
-    p1 <- ggplot2::ggplot(blup, aes(x = Predicted, y = GEN)) + geom_point(stat = "identity",
-        aes(col = Mean), size = size.shape) + geom_errorbarh(aes(xmin = Predicted -
-        CI, xmax = Predicted + CI), size = size.err.bar, height = height.err.bar) +
-        scale_color_manual(name = "Average", values = col.shape, labels = c("Above",
-            "Below")) + labs(x = x.lab, y = y.lab) + geom_vline(xintercept = mean(blup$Predicted)) +
-        scale_x_continuous(limits = x.lim, breaks = x.breaks) + theme %+replace% theme(axis.text = element_text(size = size.tex.lab,
-        colour = "black"), axis.title = element_text(size = size.tex.lab, colour = "black"))
+    p1 <- ggplot2::ggplot(blup, aes(x = Predicted, y = GEN)) +
+        geom_point(stat = "identity", aes(col = Mean), size = size.shape) +
+        geom_errorbarh(aes(xmin = Predicted -CI, xmax = Predicted + CI), size = size.err.bar, height = height.err.bar) +
+        scale_color_manual(name = "Average", values = col.shape, labels = c("Above", "Below")) +
+        labs(x = x.lab, y = y.lab) +
+        geom_vline(xintercept = mean(blup$Predicted)) +
+        scale_x_continuous(limits = x.lim, breaks = x.breaks) +
+        theme %+replace% theme(axis.text = element_text(size = size.tex.lab,colour = "black"),
+                               axis.title = element_text(size = size.tex.lab, colour = "black"))
 
 
     if (export == F | FALSE) {
@@ -96,9 +98,9 @@ plot_blup <- function(x, prob = 0.05, export = FALSE, file.type = "pdf", file.na
     if (file.type == "tiff") {
         if (is.null(file.name)) {
             tiff(filename = "BLUPs genotypes.tiff", width = width, height = height,
-                units = "in", compression = "lzw", res = resolution)
+                 units = "in", compression = "lzw", res = resolution)
         } else tiff(filename = paste0(file.name, ".tiff"), width = width, height = height,
-            units = "in", compression = "lzw", res = resolution)
+                    units = "in", compression = "lzw", res = resolution)
         plot(p1)
         dev.off()
     }
