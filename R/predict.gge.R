b26b407e9837452eaf1eaaaad471bad641b8d898
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-07-18 17:37:45 -0300

    Predict a two-way table based on GGE model

--- R/predict.gge.R
+++ R/predict.gge.R
@@ -29,31 +29,34 @@
 #' predict(mod)
 #'
 predict.gge = function(object, naxis = 2, output = "wide", ...){
+  if(!class(object) ==  "gge"){
+    stop("The object must be of class 'gge'.")
+  }
   if(naxis > min(dim(object$coordenv))){
     stop("The number of principal components cannot be greater than min(g, e), in this case ", min(dim(object$coordenv)))
   }
   # SVP
-  if(object$svp == "environment"){
+  if(object$svp == "environment" | object$svp == 2){
     pred = (object$coordgen[,1:naxis] * (object$d)) %*% t(object$coordenv[,1:naxis])
   }
-  if(object$svp == "genotype"){
+  if(object$svp == "genotype" | object$svp == 1){
     pred = (object$coordgen[,1:naxis] %*% t(object$coordenv[,1:naxis]* (object$d)))
   }
-  if(object$svp == "symmetrical"){
+  if(object$svp == "symmetrical" | object$svp == 3){
     pred = (object$coordgen[,1:naxis] %*% t(object$coordenv[,1:naxis]))
   }
   # Scaling
-  if(object$scaling == "sd"){
+  if(object$scaling == "sd" | object$scaling == 1){
     pred = sweep(pred, 2, object$scale_val, FUN = "*")
   }
   # Centering
-  if(object$centering == "global"){
+  if(object$centering == "global" | object$centering == 1){
     pred = pred + object$grand_mean
   }
-  if(object$centering == "environment"){
+  if(object$centering == "environment" | object$centering == 2){
     pred = sweep(pred, 2, object$mean_env, FUN = "+")
   }
-  if(object$centering == "double"){
+  if(object$centering == "double" | object$centering == 3){
     for (i in 1:nrow(pred)) {
       for (j in 1:ncol(pred)) {
         pred[i, j] <- pred[i, j] - object$grand_mean + object$mean_env[j] + object$mean_gen[i]
