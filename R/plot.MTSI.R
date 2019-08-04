3fc850375035f130a0c3d16c606eb1c59ce1b0a4
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-08-04 17:56:29 -0300

    Plot the multi-trait stability index

--- R/plot.MTSI.R
+++ R/plot.MTSI.R
@@ -8,7 +8,7 @@
 #' total number of genotypes.
 #' @param radar Logical argument. If true (default) a radar plot is generated
 #' after using \code{coord_polar()}.
-#' @param size.point The size of the point in graphic.
+#' @param size.point The size of the point in graphic. Defaults to 2.5.
 #' @param col.sel The colour for selected genotypes.
 #' @param col.nonsel The colour for nonselected genotypes.
 #' @param ... Other arguments of the function.
@@ -36,33 +36,43 @@
 #'}
 #'
 #'
-plot.mtsi <- function(x, SI = 15, radar = TRUE, size.point = 2, col.sel = "red", col.nonsel = "black",
+plot.mtsi <- function(x, SI = 15, radar = TRUE, size.point = 2.5, col.sel = "red", col.nonsel = "black",
     ...) {
 
     if (!class(x) == "mtsi") {
         stop("The object 'x' is not of class 'mtsi'")
     }
-    data <- data.frame(MTSI = x$MTSI, Genotype = names(x$MTSI))
-    ngsel <- round(nrow(data) * (SI/100), 0)
-    data$sel <- "Selected"
-    data$sel[(ngsel + 1):nrow(data)] <- "Nonselected"
+    data <- tibble(MTSI = x$MTSI,
+                   Genotype = names(x$MTSI),
+                   sel = "Selected")
+    data$sel[(round(nrow(data) * (SI/100), 0) + 1):nrow(data)] <- "Nonselected"
     cutpoint <- max(subset(data, sel == "Selected")$MTSI)
-    p <- ggplot(data = data, aes(x = reorder(Genotype, -MTSI), y = MTSI)) + geom_hline(yintercept = cutpoint,
-        col = col.sel) + geom_path(colour = "black", group = 1) + geom_point(size = size.point,
-        aes(colour = sel)) + scale_x_discrete() + scale_y_reverse() + theme_minimal() +
-        theme(legend.position = "bottom", legend.title = element_blank(), axis.title.x = element_blank(),
-            panel.border = element_blank(), axis.text = element_text(colour = "black")) +
-        labs(y = "Multitrait stability index") + scale_color_manual(values = c(col.nonsel,
-        col.sel))
+    p <- ggplot(data = data, aes(x = reorder(Genotype, -MTSI), y = MTSI)) +
+        geom_hline(yintercept = cutpoint, col = col.sel) +
+        geom_path(colour = "black", group = 1) +
+        geom_point(size = size.point, aes(fill = sel), shape = 21,  colour = "black") +
+        scale_x_discrete() +
+        scale_y_reverse() +
+        theme_minimal()+
+        theme(legend.position = "bottom",
+              legend.title = element_blank(),
+              axis.title.x = element_blank(),
+              panel.border = element_blank(),
+              axis.text = element_text(colour = "black")) +
+        labs(y = "Multitrait stability index") +
+        scale_fill_manual(values = c(col.nonsel, col.sel))
+
     if (radar == TRUE) {
+
         sequence_length = length(unique(data$Genotype))
         first_sequence = c(1:(sequence_length%/%2))
         second_sequence = c((sequence_length%/%2+1):sequence_length)
         first_angles = c(90 - 180/length(first_sequence) * first_sequence)
         second_angles = c(-90 - 180/length(second_sequence) * second_sequence)
         p <- p + coord_polar() +
-            theme(axis.text.x = element_text(angle= c(first_angles,second_angles)),
-                  legend.margin = margin(0,0,-20,-20))
+            theme(axis.text.x = element_text(angle= c(first_angles, second_angles),
+                                             vjust = 1),
+                  legend.margin = margin(-120,0,0,0))
     }
     return(p)
 }
