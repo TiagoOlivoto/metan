79f312b9e02f5605ee364cf818307d7f1f5a4dce
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-08-09 02:53:46 -0300

    Coerce to an object of class lpcor

--- R/as.lpcor.R
+++ R/as.lpcor.R
@@ -19,15 +19,17 @@
 #' is.lpcor(lpdata)
 #'
 #'
-as.lpcor = function(...){
-  data = list(...)
-  if(length(which(sapply(data, function(x) identical(nrow(x), ncol(x)))==TRUE)) != length(data)){
+as.lpcor <- function(...) {
+  data <- list(...)
+  if (length(which(sapply(data, function(x) identical(nrow(x),
+                                                      ncol(x))) == TRUE)) != length(data)) {
     stop(call. = FALSE, "All matrices in the list must be a square matrix. Please, check and fix.")
   }
-  if (length(unique(unique(sapply(data, function(x) dim(x)))[1,1:length(data)])) != 1){
+  if (length(unique(unique(sapply(data, function(x) dim(x)))[1,
+                                                             1:length(data)])) != 1) {
     stop(call. = FALSE, "All matrices in the list must be the same dimension. Please, check and fix.")
   }
-  data =  lapply(data, function(x) as.matrix(x))
-  names(data) = paste("mat", 1:length(data), sep = "_")
+  data <- lapply(data, function(x) as.matrix(x))
+  names(data) <- paste("mat", 1:length(data), sep = "_")
   invisible(structure(data, class = "lpcor"))
 }
