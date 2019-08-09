31da72938f134ec62010cacbdfbfc03404413c5e
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-08-09 03:05:21 -0300

    Split a data frame by factors

--- R/split_factors.R
+++ R/split_factors.R
@@ -21,6 +21,7 @@
 #' of the grouping variables.
 #' @author Tiago Olivoto \email{tiagoolivoto@@gmail.com}
 #' @export
+#' @importFrom rlang  eval_bare  expr set_names
 #' @examples
 #' library(metan)
 #'
@@ -28,26 +29,25 @@
 #' g2 = split_factors(data_ge, ENV, keep_factors = TRUE)
 #'
 #'
-split_factors = function(.data, ..., keep_factors = FALSE, verbose = TRUE) {
+#'
+split_factors <- function(.data, ..., keep_factors = FALSE, verbose = TRUE) {
   grouped <- group_by(.data, ...)
-  names <- rlang::eval_bare(rlang::expr(paste(!!!group_keys(grouped), sep = " / ")))
-  gd =   grouped %>%
-    group_split(keep = TRUE) %>%
-    rlang::set_names(names)
-  if (keep_factors == FALSE){
-    if (verbose == TRUE){
-      if (sum(lapply(gd[[1]], is.factor)==TRUE)>0){
-      message("The columns ", paste0(collapse = " ", names(gd[[1]][ , unlist(lapply(gd[[1]], is.factor)) ])),
-            " where deleted. Use 'keep_factors = TRUE' to keep this columns in the grouped data. ")
+  names <- eval_bare(expr(paste(!!!group_keys(grouped), sep = " | ")))
+  gd <- grouped %>% group_split(keep = TRUE) %>% set_names(names)
+  if (keep_factors == FALSE) {
+    if (verbose == TRUE) {
+      if (sum(lapply(gd[[1]], is.factor) == TRUE) > 0) {
+        message("The columns ", paste0(collapse = " ", names(gd[[1]][,
+                                                                     unlist(lapply(gd[[1]], is.factor))])), " where deleted. Use 'keep_factors = TRUE' to keep this columns in the grouped data. ")
       }
     }
-    gd = lapply(gd, function(x){
-      x[ , unlist(lapply(x, is.numeric))] %>% as_tibble()
+    gd <- lapply(gd, function(x) {
+      x[, unlist(lapply(x, is.numeric))] %>% as_tibble()
     })
-  } else{
-    gd = lapply(gd, function(x){
-          as_tibble(x)
-         })
+  } else {
+    gd <- lapply(gd, function(x) {
+      as_tibble(x)
+    })
   }
   return(structure(gd, class = "split_factors"))
 }
