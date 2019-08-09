43ee2d50ffdde4ba66e8ef96a674e5fa296f22c6
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-08-09 02:57:54 -0300

    Pairwise combinations of variables

--- R/comb_vars.R
+++ R/comb_vars.R
@@ -6,9 +6,9 @@
 #'
 #' @param .data A matrix of data with, say, p columns.
 #' @param order The order on how the results will appear in the output. Default
-#' is \code{order = "first"}. In this case, assuming that .data has four
+#' is \code{order = 'first'}. In this case, assuming that .data has four
 #' columns, namely, \code{V1, V2, V3, V4}, the order of columns in the output
-#' will be \code{V1.V2, V1.V3, V1.V4, V2.V3, V2.V4, V3.V4}. If \code{order = "second"},
+#' will be \code{V1.V2, V1.V3, V1.V4, V2.V3, V2.V4, V3.V4}. If \code{order = 'second'},
 #' the result will be then \code{V1.V2, V1.V3, V2.V3, V1.V4, V2.V4, V3.V4}.
 #' @param FUN The function that will be applied to each combination. The
 #' default is \code{+}, i.e., V1 + V2.
@@ -26,29 +26,31 @@
 #'                   D = rnorm(n = 5, mean = 1100, sd = 200),
 #'                   E = rnorm(n = 5, mean = 2, sd = 1))
 #' comb_vars(data)
-#' comb_vars(data, FUN = "*", order = "second")
+#' comb_vars(data, FUN = '*', order = 'second')
 #' }
 #'
-comb_vars = function(.data, order = "first", FUN = "+", verbose = TRUE){
-  FUN = match.fun(FUN)
+comb_vars <- function(.data, order = "first", FUN = "+", verbose = TRUE) {
+  FUN <- match.fun(FUN)
   if (!order %in% c("first", "second")) {
     stop("The orde must be one of 'first' or 'second'.")
   }
-  if (verbose == TRUE){
-    if (sum(lapply(.data, is.factor) == TRUE)>0){
-    message("The columns factors ", paste0(collapse = " ", names(.data[ , unlist(lapply(.data, is.factor))])),
-            " where deleted. Only the numeric variables were used.")
+  if (verbose == TRUE) {
+    if (sum(lapply(.data, is.factor) == TRUE) > 0) {
+      message("The columns factors ", paste0(collapse = " ",
+                                             names(.data[, unlist(lapply(.data, is.factor))])),
+              " where deleted. Only the numeric variables were used.")
     }
   }
-  x = .data[ , unlist(lapply(.data, is.numeric))] %>% as.data.frame()
-  cb = data.frame(t(combn(ncol(x), 2)))
-  if (order == "second"){
-  cb %<>% arrange(X2)
+  x <- .data[, unlist(lapply(.data, is.numeric))] %>% as.data.frame()
+  cb <- data.frame(t(combn(ncol(x), 2)))
+  if (order == "second") {
+    cb %<>% arrange(X2)
   }
-  nvars = data.frame(matrix(nrow = nrow(x), ncol = nrow(cb)))
-  for (i in 1:nrow(cb)){
-    nvars[, i] = FUN(x[[cb[i, 1]]], x[[cb[i, 2]]])
-    colnames(nvars)[[i]] =  paste(colnames(x[cb[i, 1]]), colnames(x[cb[i, 2]]), sep = "x")
+  nvars <- data.frame(matrix(nrow = nrow(x), ncol = nrow(cb)))
+  for (i in 1:nrow(cb)) {
+    nvars[, i] <- FUN(x[[cb[i, 1]]], x[[cb[i, 2]]])
+    colnames(nvars)[[i]] <- paste(colnames(x[cb[i, 1]]),
+                                  colnames(x[cb[i, 2]]), sep = "x")
   }
   return(as_tibble(nvars, rownames = NA))
 }
