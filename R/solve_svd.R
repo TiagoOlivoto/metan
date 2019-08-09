9877dc2e143415144afe9d815ab81aaa41c22047
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-08-09 02:53:09 -0300

    Pseudoinverse of a square matrix

--- R/solve_svd.R
+++ R/solve_svd.R
@@ -7,18 +7,18 @@
 #' @param tolerance The tolerance to consider an eigenvalue equals to zero.
 #' @author Tiago Olivoto, \email{tiagoolivoto@@gmail.com}
 #' @export
-
-solve_svd = function(x, tolerance = 2.220446e-16){
-  if (dim(x)[1] - dim(x)[2] !=0){
+solve_svd <- function(x, tolerance = 2.220446e-16) {
+  if (dim(x)[1] - dim(x)[2] != 0) {
     stop("The object 'x' must be a square matrix")
   }
-  s = svd(as.matrix(x))
+  s <- svd(as.matrix(x))
   posi <- s$d > max(tolerance * s$d[1], 0)
-  if (all(posi)){
+  if (all(posi)) {
     s$v %*% (1/s$d * t(s$u))
-  } else if (!any(posi)){
+  } else if (!any(posi)) {
     array(0, dim(x)[2L:1L])
   } else {
-   s$v[, posi, drop = FALSE] %*% ((1/s$d[posi]) * t(s$u[, posi, drop = FALSE]))
+    s$v[, posi, drop = FALSE] %*% ((1/s$d[posi]) * t(s$u[,
+                                                         posi, drop = FALSE]))
   }
 }
