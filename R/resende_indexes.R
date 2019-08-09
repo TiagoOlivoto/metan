0c7ec05b3178a7c4dbba9b6164d41797c7c581d7
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-08-09 03:06:45 -0300

    Stability indexes based on a mixed-effect model

--- R/resende_indexes.R
+++ R/resende_indexes.R
@@ -73,34 +73,32 @@ Resende_indexes <- function(.data) {
     if (!is(.data, "waasb")) {
         stop("The object '.data' must be an object of class \"waasb\"")
     }
-
     # Helper functions
     hmean_fun <- function(x) {
         hmean <- length(x)/sum(1/x)
         return(hmean)
     }
-
     listres <- list()
-
     for (var in 1:length(.data)) {
-
         gge <- .data[[var]][["BLUPgge"]]
         # Harmonic mean
-        GEPRED = make_mat(gge, GEN, ENV, Predicted)
-        HMGV <- data.frame(HMGV = apply(GEPRED, 1, FUN = hmean_fun), HMGV_order = rank(-apply(GEPRED,
-            1, FUN = hmean_fun)))
+        GEPRED <- make_mat(gge, GEN, ENV, Predicted)
+        HMGV <- data.frame(HMGV = apply(GEPRED, 1, FUN = hmean_fun),
+                           HMGV_order = rank(-apply(GEPRED, 1, FUN = hmean_fun)))
         ## Relative performance
         y <- .data[[var]][["MeansGxE"]]
-
         GEMEAN <- make_mat(y, GEN, ENV, Y)
         ovmean <- mean(y$Y)
         mean_env <- apply(GEMEAN, 2, FUN = mean)
-        RPGV <- data.frame(RPGV = apply(t(t(GEPRED)/mean_env), 1, mean))
-        RPGV_data <- dplyr::mutate(RPGV, GY_RPGV = RPGV * ovmean, RPGV_order = rank(-GY_RPGV))
+        RPGV <- data.frame(RPGV = apply(t(t(GEPRED)/mean_env),
+                                        1, mean))
+        RPGV_data <- dplyr::mutate(RPGV, GY_RPGV = RPGV * ovmean,
+                                   RPGV_order = rank(-GY_RPGV))
         ## Harmonic mean of Relative performance
-        HMRPGV <- data.frame(HMRPGV = apply(t(t(GEPRED)/mean_env), 1, hmean_fun))
-        HMRPGV_data <- dplyr::mutate(HMRPGV, GY_HMRPGV = HMRPGV * ovmean, HMRPGV_order = rank(-GY_HMRPGV))
-
+        HMRPGV <- data.frame(HMRPGV = apply(t(t(GEPRED)/mean_env),
+                                            1, hmean_fun))
+        HMRPGV_data <- dplyr::mutate(HMRPGV, GY_HMRPGV = HMRPGV *
+                                         ovmean, HMRPGV_order = rank(-GY_HMRPGV))
         temp <- data.frame(cbind(HMGV, RPGV_data, HMRPGV_data))
         rownames(temp) <- rownames(RPGV)
         listres[[paste(names(.data[var]))]] <- as_tibble(temp)
