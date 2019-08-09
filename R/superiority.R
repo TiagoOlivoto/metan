a6a74f5b098e38c562896c00e752f8fe3a3e1eef
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-08-09 03:07:23 -0300

    Lin e Binns' superiority index

--- R/superiority.R
+++ R/superiority.R
@@ -31,67 +31,56 @@
 #'                  resp = PH)
 #'
 #'
-superiority = function(.data,
-                       env,
-                       gen,
-                       rep,
-                       resp,
-                       verbose = TRUE){
+superiority <- function(.data, env, gen, rep, resp, verbose = TRUE) {
   datain <- .data
   GEN <- factor(eval(substitute(gen), eval(datain)))
   ENV <- factor(eval(substitute(env), eval(datain)))
   REP <- factor(eval(substitute(rep), eval(datain)))
   listres <- list()
   d <- match.call()
-  nvar <- as.numeric(ifelse(length(d$resp) > 1, length(d$resp) - 1, length(d$resp)))
+  nvar <- as.numeric(ifelse(length(d$resp) > 1, length(d$resp) -
+                              1, length(d$resp)))
   for (var in 2:length(d$resp)) {
     if (length(d$resp) > 1) {
       Y <- eval(substitute(resp)[[var]], eval(datain))
-      varnam = paste(d$resp[var])
+      varnam <- paste(d$resp[var])
     } else {
       Y <- eval(substitute(resp), eval(datain))
-      varnam = paste(d$resp)
+      varnam <- paste(d$resp)
     }
     data <- data.frame(ENV, GEN, REP, Y)
-    names(data) = c("ENV", "GEN", "REP", "mean")
-    ge_mean =  data  %>%
-      dplyr::group_by(ENV, GEN) %>%
-      dplyr::summarise(mean = mean(mean))
-    environments = data  %>%
-      dplyr::group_by(ENV) %>%
-      dplyr::summarise(Mean = mean(mean))
-    environments = mutate(environments,
-                          index = Mean - mean(environments$Mean),
-                          class = ifelse(index < 0, "unfavorable", "favorable")) %>%
+    names(data) <- c("ENV", "GEN", "REP", "mean")
+    ge_mean <- data %>% dplyr::group_by(ENV, GEN) %>% dplyr::summarise(mean = mean(mean))
+    environments <- data %>% dplyr::group_by(ENV) %>% dplyr::summarise(Mean = mean(mean))
+    environments <- mutate(environments, index = Mean - mean(environments$Mean),
+                           class = ifelse(index < 0, "unfavorable", "favorable")) %>%
       as.data.frame()
-    data = suppressMessages(left_join(data, environments %>% select(ENV, class)))
-
-    lin_fun = function(mat){
-    P <- apply(mat, 1, function(x) {
-      sum((x - apply(mat, 2, max))^2) / (2 * length(x))
-    })
-    return(P)
+    data <- suppressMessages(left_join(data, environments %>%
+                                         select(ENV, class)))
+    lin_fun <- function(mat) {
+      P <- apply(mat, 1, function(x) {
+        sum((x - apply(mat, 2, max))^2)/(2 * length(x))
+      })
+      return(P)
     }
-
-    mat_g  = make_mat(data, row = GEN, col = ENV, value = mean)
-    ge_mf = subset(data, class == "favorable")
-    mat_f  = dplyr::select_if(make_mat(ge_mf, row = GEN, col = ENV, value = mean), function(x) !any(is.na(x)))
-    ge_mu = subset(data, class == "unfavorable")
-    mat_u  = dplyr::select_if(make_mat(ge_mu, row = GEN, col = ENV, value = mean), function(x) !any(is.na(x)))
-    temp = list(environments = tibble(environments),
-                index = tibble(Genotypes = rownames(mat_g),
-                               Pi_all = lin_fun(mat_g),
-                               Or_a = rank(lin_fun(mat_g)),
-                               Pi_favorable = lin_fun(mat_f),
-                               Or_f = rank(lin_fun(mat_f)),
-                               Pi_unfavorable = lin_fun(mat_u),
-                               Or_u = rank(lin_fun(mat_u))))
-    rownames(temp) = NULL
+    mat_g <- make_mat(data, row = GEN, col = ENV, value = mean)
+    ge_mf <- subset(data, class == "favorable")
+    mat_f <- dplyr::select_if(make_mat(ge_mf, row = GEN,
+                                       col = ENV, value = mean), function(x) !any(is.na(x)))
+    ge_mu <- subset(data, class == "unfavorable")
+    mat_u <- dplyr::select_if(make_mat(ge_mu, row = GEN,
+                                       col = ENV, value = mean), function(x) !any(is.na(x)))
+    temp <- list(environments = tibble(environments), index = tibble(Genotypes = rownames(mat_g),
+                                                                     Pi_all = lin_fun(mat_g), Or_a = rank(lin_fun(mat_g)),
+                                                                     Pi_favorable = lin_fun(mat_f), Or_f = rank(lin_fun(mat_f)),
+                                                                     Pi_unfavorable = lin_fun(mat_u), Or_u = rank(lin_fun(mat_u))))
+    rownames(temp) <- NULL
     if (length(d$resp) > 1) {
       listres[[paste(d$resp[var])]] <- temp
       if (verbose == TRUE) {
-        cat("Evaluating variable", paste(d$resp[var]), round((var - 1)/(length(d$resp) -
-                                                                          1) * 100, 1), "%", "\n")
+        cat("Evaluating variable", paste(d$resp[var]),
+            round((var - 1)/(length(d$resp) - 1) * 100,
+                  1), "%", "\n")
       }
     } else {
       listres[[paste(d$resp)]] <- temp
