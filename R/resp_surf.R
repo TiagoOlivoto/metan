788f68dee8ab95225f252f67337e0b0e0263cd3d
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-08-09 02:50:46 -0300

    Response surface model

--- R/resp_surf.R
+++ R/resp_surf.R
@@ -17,142 +17,155 @@
 #' @export
 #' @author Tiago Olivoto \email{tiagoolivoto@@gmail.com}
 #'
-resp_surf = function(.data,
-                     factor1,
-                     factor2,
-                     rep = NULL,
-                     resp,
-                     prob = 0.05,
-                     verbose = TRUE) {
-if(!missing(rep)){
-data = .data  %>%
-        dplyr::select(!!dplyr::enquo(factor1),
-                      !!dplyr::enquo(factor2),
-                      !!dplyr::enquo(rep),
-                      !!dplyr::enquo(resp)) %>%
-  mutate_at(c(1:3), as.factor)
-names = colnames(data)
-A = names[1]
-D = names[2]
-Bloco = names[3]
-Resp = names[4]
-F1 = as.formula(paste0(Resp, "~", paste(Bloco),"+", paste(A), "+", paste(D), "+", paste(A), "*", paste(D)))
-ANOVA = aov(F1, data = data)
-} else{
-  data = .data  %>%
-    dplyr::select(!!dplyr::enquo(factor1),
-                  !!dplyr::enquo(factor2),
-                  !!dplyr::enquo(resp)) %>%
-    mutate_at(c(1:2), as.factor)
-  names = colnames(data)
-  A = names[1]
-  D = names[2]
-  Resp = names[3]
-
-}
-  F2 = as.formula(paste0(Resp, " ~",  paste(A),"*", paste(D), "+", "I(",paste(A),"^2)", "+","I(",paste(D), "^2)"))
-  SurfMod = lm(F2, data=.data)
-  B0 = SurfMod$coef[1]
-  B1 = SurfMod$coef[2]
-  B2 = SurfMod$coef[3]
-  B11 = SurfMod$coef[4]
-  B22 = SurfMod$coef[5]
-  B12 = SurfMod$coef[6]
-  B12c = B12/2
-  P = cbind(c(B11,B12c), c(B12c,B22))
-  P = as.matrix(P)
-  invA = solve(P)
-  X = cbind(c(B1, B2))
-  Pontos = -0.5*(invA %*% X)
-  dA = Pontos[1]
-  dD = Pontos[2]
-  AV1 = eigen(P)$values[1]
-  AV2 = eigen(P)$values[2]
-  results = dplyr::mutate(data,
-                          predicted = SurfMod$fitted.values,
-                          residuals = SurfMod$residuals)
-
-if (verbose == TRUE) {
-  if(!missing(rep)){
-  cat("-----------------------------------------------------------------\n")
-  cat("Result for the analysis of variance", "\n")
-  cat("Model: Y = m + bk + Ai + Dj + (AD)ij + eijk", "\n")
-  cat("-----------------------------------------------------------------\n")
-  print(summary(ANOVA))
-  Norm = shapiro.test(ANOVA$residuals)
-  cat("-----------------------------------------------------------------\n")
-  cat("Shapiro-Wilk's test for normality of residuals:", "\n")
-  cat("-----------------------------------------------------------------\n")
-  cat("W = ",Norm$statistic, "p-valor = ", Norm$p.value, "\n")
-  if (Norm$p.value>0.05){
-    cat("According to S-W test, residuals may be considered normal.","\n")
-  }
-  }
-  cat("-----------------------------------------------------------------\n")
-  cat("Anova table for the response surface model", "\n")
-  cat("-----------------------------------------------------------------\n")
-  print(anova(SurfMod))
-  cat("-----------------------------------------------------------------\n")
-  cat("Model equation for response surface model", "\n")
-  cat("Y = B0 + B1*A + B2*D + B11*A^2 + B22*D^2 + B12*A*D", "\n")
-  cat("-----------------------------------------------------------------\n")
-  cat("Estimated parameters", "\n")
-  cat(paste0("B0: ", format(round(B0,7), nsmall = 7),"\n"))
-  cat(paste0("B1: ", format(round(B1,7), nsmall = 7),"\n"))
-  cat(paste0("B2: ", format(round(B2,7), nsmall = 7),"\n"))
-  cat(paste0("B11: ", format(round(B11,7), nsmall = 7),"\n"))
-  cat(paste0("B22: ", format(round(B22,7), nsmall = 7),"\n"))
-  cat(paste0("B12: ", format(round(B12,7), nsmall = 7),"\n"))
-  cat("-----------------------------------------------------------------\n")
-  cat("Matrix of parameters (A)","\n")
-  cat("-----------------------------------------------------------------\n")
-  cat(paste0(format(round(P[1,1],7), nsmall = 7)),"  ", format(round(P[1,2],7), nsmall = 7),"\n")
-  cat(paste0(format(round(P[2,1],7), nsmall = 7)),"  ", format(round(P[2,2],7), nsmall = 7),"\n")
-  cat("-----------------------------------------------------------------\n")
-  cat("Inverse of the matrix A (invA)","\n")
-  cat(paste0(format(round(invA[1,1],7), nsmall = 7)),"  ", format(round(invA[1,2],7), nsmall = 7),"\n")
-  cat(paste0(format(round(invA[2,1],7), nsmall = 7)),"  ", format(round(invA[2,2],7), nsmall = 7),"\n")
-  cat("-----------------------------------------------------------------\n")
-  cat("Vetor of parameters B1 e B2 (X)","\n")
-  cat("-----------------------------------------------------------------\n")
-  cat(paste0("B1: ", format(round(B1,7), nsmall = 7),"\n"))
-  cat(paste0("B2: ", format(round(B2,7), nsmall = 7),"\n"))
-  cat("-----------------------------------------------------------------\n")
-  cat("Equation for the optimal points (A and D)","\n")
-  cat("-----------------------------------------------------------------\n")
-  cat("-0.5*(invA*X)")
-  cat(paste0("\nEigenvalue 1: " ,round(AV1,6),
-             "\nEigenvalue 2: " ,round(AV2,6)))
-  cat("\n")
-  if (AV1 > 0 && AV2 > 0) {
-    cat(paste0("Stacionary point is minimum!"))
-  } else if (AV1 < 0 && AV2 < 0) {
-    cat(paste0("Stacionary point is maximum!"))
-  } else
-    cat(paste0("The stacionary point is outside the intervals of the trataments"))
-  cat("\n")
-  cat("-----------------------------------------------------------------\n")
-  cat("Stacionary point obtained with the following original units:", "\n")
-  cat("-----------------------------------------------------------------\n")
-  cat(paste0("Optimal dose (",A,"): ", round(dA,4),"\n"))
-  cat(paste0("Optimal dose (",D,"): ", round(dD,4),"\n"))
-  cat("-----------------------------------------------------------------\n")
-  cat("Fitted model", "\n")
-  cat("-----------------------------------------------------------------\n")
-  cat(paste0("A = ", A,"\n"))
-  cat(paste0("D = ", D,"\n"))
-  cat(paste0("y = " , round(B0,5), "+",round(B1,5),"A+",round(B2,5),"D+" , round(B11,5),"A^2+" , round(B22,5),"D^2+" , round(B12,5),"A*D","\n"))
-  cat("-----------------------------------------------------------------\n")
-  pvalor.shapiro = shapiro.test(results$residuals)$p.value
-  cat("Shapiro-Wilk normality test\n")
-  cat("p-value: ", pvalor.shapiro, "\n")
-  if (pvalor.shapiro < 0.05) {
-    cat("WARNING: at 5% of significance, residuals can not be considered normal!", "\n")
-    cat("------------------------------------------------------------------")
+resp_surf <- function(.data, factor1, factor2, rep = NULL, resp,
+                      prob = 0.05, verbose = TRUE) {
+  if (!missing(rep)) {
+    data <- .data %>% dplyr::select(!!dplyr::enquo(factor1),
+                                    !!dplyr::enquo(factor2), !!dplyr::enquo(rep), !!dplyr::enquo(resp)) %>%
+      mutate_at(c(1:3), as.factor)
+    names <- colnames(data)
+    A <- names[1]
+    D <- names[2]
+    Bloco <- names[3]
+    Resp <- names[4]
+    F1 <- as.formula(paste0(Resp, "~", paste(Bloco), "+",
+                            paste(A), "+", paste(D), "+", paste(A), "*", paste(D)))
+    ANOVA <- aov(F1, data = data)
   } else {
-    cat("According to Shapiro-Wilk normality test at 5% of significance, residuals can be considered normal.", "\n")
-    cat("------------------------------------------------------------------\n")
+    data <- .data %>% dplyr::select(!!dplyr::enquo(factor1),
+                                    !!dplyr::enquo(factor2), !!dplyr::enquo(resp)) %>%
+      mutate_at(c(1:2), as.factor)
+    names <- colnames(data)
+    A <- names[1]
+    D <- names[2]
+    Resp <- names[3]
   }
-}
-  invisible(structure(list(results = results, model = SurfMod),  class = "resp_surf"))
+  F2 <- as.formula(paste0(Resp, " ~", paste(A), "*", paste(D),
+                          "+", "I(", paste(A), "^2)", "+", "I(", paste(D), "^2)"))
+  SurfMod <- lm(F2, data = .data)
+  B0 <- SurfMod$coef[1]
+  B1 <- SurfMod$coef[2]
+  B2 <- SurfMod$coef[3]
+  B11 <- SurfMod$coef[4]
+  B22 <- SurfMod$coef[5]
+  B12 <- SurfMod$coef[6]
+  B12c <- B12/2
+  P <- cbind(c(B11, B12c), c(B12c, B22))
+  P <- as.matrix(P)
+  invA <- solve(P)
+  X <- cbind(c(B1, B2))
+  Pontos <- -0.5 * (invA %*% X)
+  dA <- Pontos[1]
+  dD <- Pontos[2]
+  AV1 <- eigen(P)$values[1]
+  AV2 <- eigen(P)$values[2]
+  results <- dplyr::mutate(data, predicted = SurfMod$fitted.values,
+                           residuals = SurfMod$residuals)
+  if (verbose == TRUE) {
+    if (!missing(rep)) {
+      cat("-----------------------------------------------------------------\n")
+      cat("Result for the analysis of variance", "\n")
+      cat("Model: Y = m + bk + Ai + Dj + (AD)ij + eijk",
+          "\n")
+      cat("-----------------------------------------------------------------\n")
+      print(summary(ANOVA))
+      Norm <- shapiro.test(ANOVA$residuals)
+      cat("-----------------------------------------------------------------\n")
+      cat("Shapiro-Wilk's test for normality of residuals:",
+          "\n")
+      cat("-----------------------------------------------------------------\n")
+      cat("W = ", Norm$statistic, "p-valor = ", Norm$p.value,
+          "\n")
+      if (Norm$p.value > 0.05) {
+        cat("According to S-W test, residuals may be considered normal.",
+            "\n")
+      }
+    }
+    cat("-----------------------------------------------------------------\n")
+    cat("Anova table for the response surface model", "\n")
+    cat("-----------------------------------------------------------------\n")
+    print(anova(SurfMod))
+    cat("-----------------------------------------------------------------\n")
+    cat("Model equation for response surface model", "\n")
+    cat("Y = B0 + B1*A + B2*D + B11*A^2 + B22*D^2 + B12*A*D",
+        "\n")
+    cat("-----------------------------------------------------------------\n")
+    cat("Estimated parameters", "\n")
+    cat(paste0("B0: ", format(round(B0, 7), nsmall = 7),
+               "\n"))
+    cat(paste0("B1: ", format(round(B1, 7), nsmall = 7),
+               "\n"))
+    cat(paste0("B2: ", format(round(B2, 7), nsmall = 7),
+               "\n"))
+    cat(paste0("B11: ", format(round(B11, 7), nsmall = 7),
+               "\n"))
+    cat(paste0("B22: ", format(round(B22, 7), nsmall = 7),
+               "\n"))
+    cat(paste0("B12: ", format(round(B12, 7), nsmall = 7),
+               "\n"))
+    cat("-----------------------------------------------------------------\n")
+    cat("Matrix of parameters (A)", "\n")
+    cat("-----------------------------------------------------------------\n")
+    cat(paste0(format(round(P[1, 1], 7), nsmall = 7)), "  ",
+        format(round(P[1, 2], 7), nsmall = 7), "\n")
+    cat(paste0(format(round(P[2, 1], 7), nsmall = 7)), "  ",
+        format(round(P[2, 2], 7), nsmall = 7), "\n")
+    cat("-----------------------------------------------------------------\n")
+    cat("Inverse of the matrix A (invA)", "\n")
+    cat(paste0(format(round(invA[1, 1], 7), nsmall = 7)),
+        "  ", format(round(invA[1, 2], 7), nsmall = 7), "\n")
+    cat(paste0(format(round(invA[2, 1], 7), nsmall = 7)),
+        "  ", format(round(invA[2, 2], 7), nsmall = 7), "\n")
+    cat("-----------------------------------------------------------------\n")
+    cat("Vetor of parameters B1 e B2 (X)", "\n")
+    cat("-----------------------------------------------------------------\n")
+    cat(paste0("B1: ", format(round(B1, 7), nsmall = 7),
+               "\n"))
+    cat(paste0("B2: ", format(round(B2, 7), nsmall = 7),
+               "\n"))
+    cat("-----------------------------------------------------------------\n")
+    cat("Equation for the optimal points (A and D)", "\n")
+    cat("-----------------------------------------------------------------\n")
+    cat("-0.5*(invA*X)")
+    cat(paste0("\nEigenvalue 1: ", round(AV1, 6), "\nEigenvalue 2: ",
+               round(AV2, 6)))
+    cat("\n")
+    if (AV1 > 0 && AV2 > 0) {
+      cat(paste0("Stacionary point is minimum!"))
+    } else if (AV1 < 0 && AV2 < 0) {
+      cat(paste0("Stacionary point is maximum!"))
+    } else cat(paste0("The stacionary point is outside the intervals of the trataments"))
+    cat("\n")
+    cat("-----------------------------------------------------------------\n")
+    cat("Stacionary point obtained with the following original units:",
+        "\n")
+    cat("-----------------------------------------------------------------\n")
+    cat(paste0("Optimal dose (", A, "): ", round(dA, 4),
+               "\n"))
+    cat(paste0("Optimal dose (", D, "): ", round(dD, 4),
+               "\n"))
+    cat("-----------------------------------------------------------------\n")
+    cat("Fitted model", "\n")
+    cat("-----------------------------------------------------------------\n")
+    cat(paste0("A = ", A, "\n"))
+    cat(paste0("D = ", D, "\n"))
+    cat(paste0("y = ", round(B0, 5), "+", round(B1, 5), "A+",
+               round(B2, 5), "D+", round(B11, 5), "A^2+", round(B22,
+                                                                5), "D^2+", round(B12, 5), "A*D", "\n"))
+    cat("-----------------------------------------------------------------\n")
+    pvalor.shapiro <- shapiro.test(results$residuals)$p.value
+    cat("Shapiro-Wilk normality test\n")
+    cat("p-value: ", pvalor.shapiro, "\n")
+    if (pvalor.shapiro < 0.05) {
+      cat("WARNING: at 5% of significance, residuals can not be considered normal!",
+          "\n")
+      cat("------------------------------------------------------------------")
+    } else {
+      cat("According to Shapiro-Wilk normality test at 5% of significance, residuals can be considered normal.",
+          "\n")
+      cat("------------------------------------------------------------------\n")
+    }
+  }
+  invisible(structure(list(results = results, model = SurfMod),
+                      class = "resp_surf"))
 }
