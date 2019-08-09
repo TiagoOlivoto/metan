39312e26360916f3b3e1dedd72b31ee7524e0d81
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-08-09 03:11:32 -0300

    Plot the response surface model

--- R/plot.resp_surf.R
+++ R/plot.resp_surf.R
@@ -17,17 +17,17 @@
 #' @method plot resp_surf
 #' @export
 #'
-plot.resp_surf <- function(x, xlab = NULL, ylab = NULL, region = TRUE, resolution  = 100, ...) {
-  data = x$model$model
-  seq = expand.grid(seq(min(unique(data[2])), max(unique(data[2])),length.out = resolution),
-                    seq(min(unique(data[3])), max(unique(data[3])),length.out = resolution))
-  names(seq) = names(data[2:3])
-  seq = mutate(seq, PRED = predict(x$model, newdata = seq))
-  mod = predict(x$model, newdata = seq)
-  xlab = ifelse(missing(xlab), names(seq[1]), xlab)
-  ylab = ifelse(missing(ylab), names(seq[2]), ylab)
-  contourplot(seq[,3] ~ seq[,1] * seq[,2], region = region,
-              xlab = xlab,
-              ylab = ylab,
-              ...)
+plot.resp_surf <- function(x, xlab = NULL, ylab = NULL, region = TRUE,
+                           resolution = 100, ...) {
+  data <- x$model$model
+  seq <- expand.grid(seq(min(unique(data[2])), max(unique(data[2])),
+                         length.out = resolution), seq(min(unique(data[3])), max(unique(data[3])),
+                                                       length.out = resolution))
+  names(seq) <- names(data[2:3])
+  seq <- mutate(seq, PRED = predict(x$model, newdata = seq))
+  mod <- predict(x$model, newdata = seq)
+  xlab <- ifelse(missing(xlab), names(seq[1]), xlab)
+  ylab <- ifelse(missing(ylab), names(seq[2]), ylab)
+  contourplot(seq[, 3] ~ seq[, 1] * seq[, 2], region = region,
+              xlab = xlab, ylab = ylab, ...)
 }
