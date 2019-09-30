262f97380aad3df6c46d91947d1c29d681d131cd
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-09-30 00:36:01 -0300

    Easily get data from a model

--- R/get_model_data.R
+++ R/get_model_data.R
@@ -4,21 +4,34 @@
 #'
 #'
 #' @param x An object created with the functions \code{\link{waas}} or \code{\link{waasb}}.
-#' @param what What should be captured from the model. The following options are allowed:
-#' \code{"blupg"} for genotype's predicted mean and \code{"blupge"} for genotype-vs-environment's
-#' predicted mean (if the object is of class \code{waasb} only); \code{"Y"} for raw means (Default);
-#' \code{"PCn"} for the scores of the nth Interaction Principal Component Axis (IPCA),
-#'  where \code{n} is the desired IPCA (e.g. \code{"PC3"}); \code{"WAASB"} for the weighted
-#'  average of the absolute scores; \code{"PctResp"} and \code{"PctWAASB"} for the rescaled
-#'  values of the response variable and WAASB, respectively; \code{"wResp"} and \code{"wWAASB"}
-#'  for weight of the response variable and stability, respectively; \code{"OrResp"} and
-#'  \code{"OrWAASB"} for the ranking regarding the response variable and WAASB, respectively;
-#'  \code{"OrPC1"} for the ranking regarding the first principal component axix; \code{"WAASBY"}
-#'  for the superiority index, \code{"OrWAASBY"} for the ranking regarding the superiority
-#'  index.
+#' @param what What should be captured from the model. See more details in \strong{Details} section.
 #' @param type Chose if the statistics must be show by genotype (\code{type = "GEN"}, default)
 #' or environment (\code{type = "ENV"}), when possible.
 #' @return A tibble showing the values of the variable chosen in argument \code{what}.
+#' @details The next options are valid for both \code{waas} and \code{waasb} objects.
+#' * \code{"Y"} for raw means (Default) .
+#' * \code{"PCn"} for the scores of the nth Interaction Principal Component Axis (IPCA) .
+#'  where * \code{n} is the desired IPCA (e.g. \code{"PC3"}) .
+#' * \code{"WAASB"}  The weighted average of the absolute scores .
+#' * \code{"PctResp"} The rescaled values of the response variable .
+#' * \code{"PctWAASB"} The rescaled values of the WAASB .
+#' * \code{"wResp"} The weight for the response variable .
+#' * \code{"wWAASB"} The weight for the stability .
+#' * \code{"OrResp"} The ranking regarding the response variable .
+#' * \code{"OrWAASB"} The ranking regarding the WAASB .
+#' * \code{"OrPC1"} The ranking regarding the first principal component axix .
+#' * \code{"WAASBY"} The superiority index WAASBY .
+#' * \code{"OrWAASBY"} The ranking regarding the superiority index .
+#'
+#'  The next options are only allowed if the object is of class \code{waasb}:
+#'
+#' * \code{"blupg"} For genotype's predicted mean.
+#' * \code{"blupge"} for genotype-vs-environment's predicted mean.
+#' * \code{"genpar"} Genetic parameters.
+#' * \code{"lrt"} The statistic for the likelihood-ratio test for random effects.
+#' * \code{"pval_lrt"}  The p-values for the likelihood-ratio test.
+#' * \code{"vcomp"} The variance components for random effects.
+#' @md
 #' @importFrom dplyr starts_with matches case_when
 #' @author Tiago Olivoto \email{tiagoolivoto@@gmail.com}
 #' @export
@@ -31,87 +44,186 @@
 #' @examples
 #'
 #' library(metan)
-#'
-#' model = waas(data_ge2,
-#'              env = ENV,
-#'              gen = GEN,
-#'              rep = REP,
+#' # Fit an AMMI model for 5 variables.
+#' AMMI <- waas(data_ge2, ENV, GEN, REP,
 #'              resp = c(PH, ED, TKW, NKR))
-#' get_model_data(model, what = "OrWAASBY")
+#'
+#' # Getting the weighted average of absolute scores
+#' get_model_data(AMMI, what = "WAASB")
+#'
+#' # And the rank for the WAASB inde.
+#' get_model_data(AMMI, what = "OrWAASB")
+#'
+#' # Fitting a mixed-effect model
+#'
+#' blup <- waasb(data_ge2, ENV, GEN, REP,
+#'               resp = c(PH, ED, TKW, NKR))
+#'
+#' # Getting p-values for likelihood-ratio test
+#' get_model_data(blup, what = "pval_lrt")
+#'
+#' # Getting the variance components
+#' get_model_data(blup, what = "vcomp")
+#'
+#' # Getting the genetic parameters
+#' get_model_data(blup, what = "genpar")
 #'
 #'
-#' waasb(data_ge2, ENV, GEN, REP,
-#'       resp = c(PH, ED, TKW, NKR)) %>%
-#' get_model_data(what = "blupg")
-#'
-#'
-
 get_model_data <- function(x, what = "Y", type = "GEN") {
-if(!class(x) %in% c("waasb", "waas")){
-  stop("Invalid input in 'x' argument. It must be one object of class 'waas' or 'waasb'")
-}
-  if(substr(what, 1, 2) == "PC"){
-    npc = ncol(x[[1]][["model"]] %>%
-                 select(starts_with("PC")) %>%
-                 select(matches("PC\\d+")))
-  npcwhat = as.numeric(substr(what, 3, nchar(what)))
-  if(npcwhat > npc){
-    stop("The number of principal components informed seems to be larger than those in the model informed in 'x'.")
+  if (!class(x) %in% c("waasb", "waas")) {
+    stop("Invalid input in 'x' argument. It must be one object of class 'waas' or 'waasb'")
   }
-}
-check = c("blupg", "blupge", "Y", "WAASB", "PctResp",
-          "PctWAASB", "wRes", "wWAASB", "OrResp", "OrWAASB",
-          "OrPC1", "WAASBY", "OrWAASBY")
-check2 = paste("PC", 1:200, sep = "")
-if(what %in% c("blupg", "blupge") && class(x) == "waas"){
-  stop("Blups can only be captured from a model fitted with the function 'waasb'.")
-}
-if(!what %in% c(check, check2)){
-  stop("The argument 'what' is invalid. Please, check the function help (?get_model_data) for more details.")
-}
+  if (substr(what, 1, 2) == "PC") {
+    npc <- ncol(x[[1]][["model"]] %>%
+      select(starts_with("PC")) %>%
+      select(matches("PC\\d+")))
+    npcwhat <- as.numeric(substr(what, 3, nchar(what)))
+    if (npcwhat > npc) {
+      stop("The number of principal components informed seems to be larger than those in the model informed in 'x'.")
+    }
+  }
 
-if(!type %in% c("GEN", "ENV")){
-  stop("Argument 'type' invalid. It must be either 'GEN' or 'ENV'.")
-}
-  if(what %in% c("WAASB", "PctWAASB", "wWAASB", "OrWAASB", "WAASBY", "OrWAASBY") && class(x) == "waas"){
-  what =  case_when(what == "WAASB" ~ "WAAS",
-                    what == "PctWAASB" ~ "PctWAAS",
-                    what == "wWAASB" ~ "wWAAS",
-                    what == "OrWAASB" ~ "OrWAAS",
-                    what == "WAASBY" ~ "WAASY",
-                    what == "OrWAASBY" ~ "OrWAASY")
+  check <- c(
+    "blupg", "blupge", "Y", "WAASB", "PctResp", "PctWAASB", "wRes", "wWAASB", "OrResp", "OrWAASB",
+    "OrPC1", "WAASBY", "OrWAASBY", "vcomp", "lrt", "details", "genpar", "pval_lrt"
+  )
+  check2 <- paste("PC", 1:200, sep = "")
+  if (!what %in% c(check, check2)) {
+    stop("The argument 'what' is invalid. Please, check the function help (?get_model_data) for more details.")
   }
-if(what %in% c("blupg", "blupge")){
-  if(what == "blupg"){
-  datt <- x[[1]][["blupGEN"]]
-  bind <- do.call(cbind, lapply(x, function(x) {
-    val <- arrange(x[["blupGEN"]], GEN)$Predicted
-  }))  %>%
-    as_tibble() %>%
-    mutate(gen = datt %>% arrange(GEN) %>% pull(GEN)) %>%
-    select(gen, everything())
+  if (what %in% c("blupg", "blupge", "vcomp", "lrt", "genpar", "pval_lrt") && class(x) != "waasb") {
+    stop("Invalid argument 'what'. It can only be used with an object of class 'waasb'. Please, check and fix.")
   }
-  if(what == "blupge"){
-    bind <- do.call(cbind, lapply(x, function(x) {
-      val <- x[["BLUPgge"]][["Predicted"]]
-    })) %>%
-      as_tibble() %>%
-      mutate(ENV = x[[1]][["BLUPgge"]][["ENV"]],
-             GEN = x[[1]][["BLUPgge"]][["GEN"]]) %>%
-      select(ENV, GEN, everything())
+  if (!type %in% c("GEN", "ENV")) {
+    stop("Argument 'type' invalid. It must be either 'GEN' or 'ENV'.")
   }
-} else{
-  bind = do.call(cbind,
-                 lapply(x, function(x){
-                   x$model[[what]]
-                 })) %>%
-    as_tibble() %>%
-    mutate(gen = x[[1]][["model"]][["Code"]],
-           type = x[[1]][["model"]][["type"]]) %>%
-    dplyr::filter(type == {{type}}) %>%
-    select(-type) %>%
-    select(gen, everything())
-}
+  if (class(x) == "waas") {
+    if (what %in% c("WAASB", "PctWAASB", "wWAASB", "OrWAASB", "WAASBY", "OrWAASBY")) {
+      what <- case_when(
+        what == "WAASB" ~ "WAAS",
+        what == "PctWAASB" ~ "PctWAAS",
+        what == "wWAASB" ~ "wWAAS",
+        what == "OrWAASB" ~ "OrWAAS",
+        what == "WAASBY" ~ "WAASY",
+        what == "OrWAASBY" ~ "OrWAASY"
+      )
+    }
+    if (what %in% c("Y", "WAAS", "PctResp", "PctWAAS", "wRes", "wWAAS",
+                    "OrResp", "OrWAAS", "OrPC1", "WAASY", "OrWAASY") | what  %in% check2) {
+      bind <- do.call(
+        cbind,
+        lapply(x, function(x) {
+          x$model[[what]]
+        })
+      ) %>%
+        as_tibble() %>%
+        mutate(
+          gen = x[[1]][["model"]][["Code"]],
+          type = x[[1]][["model"]][["type"]]
+        ) %>%
+        dplyr::filter(type == {
+          {
+            type
+          }
+        }) %>%
+        select(-type) %>%
+        select(gen, everything())
+    }
+    if (what == "details") {
+      bind <- do.call(cbind, lapply(x, function(x) {
+        val <- x[["Details"]][["Values"]] %>% as.character()
+      })) %>%
+        as_tibble() %>%
+        mutate(Parameters = x[[1]][["Details"]][["Parameters"]]) %>%
+        select(Parameters, everything())
+    }
+  }
+  if (class(x) == "waasb") {
+    if (what %in% c(
+      "Y", "WAASB", "PctResp", "PctWAASB", "wRes", "wWAASB", "OrResp",
+      "OrWAASB", "OrPC1", "WAASBY", "OrWAASBY"
+    )) {
+      bind <- do.call(
+        cbind,
+        lapply(x, function(x) {
+          x$model[[what]]
+        })
+      ) %>%
+        as_tibble() %>%
+        mutate(
+          gen = x[[1]][["model"]][["Code"]],
+          type = x[[1]][["model"]][["type"]]
+        ) %>%
+        dplyr::filter(type == {
+          {
+            type
+          }
+        }) %>%
+        select(-type) %>%
+        select(gen, everything())
+    }
+    if (what == "vcomp") {
+      bind <- do.call(cbind, lapply(x, function(x) {
+        val <- x[["random"]][["Variance"]]
+      })) %>%
+        as_tibble() %>%
+        mutate(Group = x[[1]][["random"]][["Group"]]) %>%
+        select(Group, everything())
+    }
+    if (what == "genpar") {
+      bind <- do.call(cbind, lapply(x, function(x) {
+        val <- x[["ESTIMATES"]][["Values"]]
+      })) %>%
+        as_tibble() %>%
+        mutate(Parameters = x[[1]][["ESTIMATES"]][["Parameters"]]) %>%
+        select(Parameters, everything())
+    }
+    if (what == "details") {
+      bind <- do.call(cbind, lapply(x, function(x) {
+        val <- x[["Details"]][["Values"]] %>% as.character()
+      })) %>%
+        as_tibble() %>%
+        mutate(Parameters = x[[1]][["Details"]][["Parameters"]]) %>%
+        select(Parameters, everything())
+    }
+    if (what == "pval_lrt") {
+      bind <- do.call(cbind, lapply(x, function(x) {
+        val <- x[["LRT"]][["Pr(>Chisq)"]]
+      })) %>%
+        as_tibble() %>%
+        mutate(model = x[[1]][["LRT"]][["model"]]) %>%
+        select(model, everything())
+    }
+    if (what == "lrt") {
+      bind <- do.call(cbind, lapply(x, function(x) {
+        val <- x[["LRT"]][["LRT"]]
+      })) %>%
+        as_tibble() %>%
+        mutate(model = x[[1]][["LRT"]][["model"]]) %>%
+        select(model, everything())
+    }
+    if (what %in% c("blupg", "blupge")) {
+      if (what == "blupg") {
+        datt <- x[[1]][["blupGEN"]]
+        bind <- do.call(cbind, lapply(x, function(x) {
+          val <- arrange(x[["blupGEN"]], GEN)$Predicted
+        })) %>%
+          as_tibble() %>%
+          mutate(gen = datt %>% arrange(GEN) %>% pull(GEN)) %>%
+          select(gen, everything())
+      }
+      if (what == "blupge") {
+        bind <- do.call(cbind, lapply(x, function(x) {
+          val <- x[["BLUPgge"]][["Predicted"]]
+        })) %>%
+          as_tibble() %>%
+          mutate(
+            ENV = x[[1]][["BLUPgge"]][["ENV"]],
+            GEN = x[[1]][["BLUPgge"]][["GEN"]]
+          ) %>%
+          select(ENV, GEN, everything())
+      }
+    }
+  }
   return(bind)
 }
-
