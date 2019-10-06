3f403f36c8aed35665d0f6b13684b9c16ce4030e
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-10-06 14:09:27 -0300

    Fix bug in selecting VIF > 10

--- R/colindiag.R
+++ R/colindiag.R
@@ -1,70 +1,88 @@
 #' Collinearity Diagnostics
 #'
-#' Perform a (multi)collinearity diagnostic of a correlation matrix of
-#' predictor variables based several indicators, as shown by Olivoto et al.
-#' (2017).
+#' Perform a (multi)collinearity diagnostic of a correlation matrix of predictor
+#' variables using several indicators, as shown by Olivoto et al. (2017).
 #'
 #'
 #' @param .data The data to be analyzed. Must be a symmetric correlation matrix
-#' or, a dataframe containing the predictor variables, or an object of class
-#' \code{split_factors}.
+#'   or, a dataframe containing the predictor variables, or an object of class
+#'   \code{split_factors}.
+#' @param ... Variables to use in the correlation. If \code{...} is null then
+#'   all the numeric variables from \code{.data} are used. It must be a single
+#'   variable name or a comma-separated list of unquoted variables names.
 #' @param n If a correlation matrix is provided, then \code{n} is the number of
-#' objects used to compute the correlation coefficients.
+#'   objects used to compute the correlation coefficients.
 #' @param verbose If \code{verbose = TRUE} then some results are shown in the
-#' console.
+#'   console.
 #' @return
 #'
 #' The following values are returned. Please, note that if a grouping variable
 #' is used, then the results are returned into a list.
 #'
-#' \item{cormat}{A symmetric Pearson's coefficient correlation matrix between
-#' the variables}
+#' * \strong{cormat} A symmetric Pearson's coefficient correlation matrix
+#' between the variables
 #'
-#' \item{corlist}{A hypothesis testing for each of the correlation
-#' coefficients}
+#' * \strong{corlist} A hypothesis testing for each of the correlation
+#' coefficients
 #'
-#' \item{evalevet}{The eigenvalues with associated eigenvectors of the
-#' correlation matrix}
+#' * \strong{evalevet} The eigenvalues with associated eigenvectors of the
+#' correlation matrix
 #'
-#' \item{VIF}{The Variance Inflation Factors, being the diagonal elements of
-#' the inverse of the correlation matrix.}
+#' * \strong{VIF} The Variance Inflation Factors, being the diagonal elements of
+#' the inverse of the correlation matrix.
 #'
-#' \item{CN}{The Condition Number of the correlation matrix, given by the ratio
-#' between the largest and smallest eigenvalue.}
+#' * \strong{CN} The Condition Number of the correlation matrix, given by the
+#' ratio between the largest and smallest eigenvalue.
 #'
-#' \item{det}{The determinant of the correlation matrix.}
+#' * \strong{det} The determinant of the correlation matrix.
 #'
-#' \item{largest_corr}{The largest correlation (in absolute value) observed.}
+#' * \strong{largest_corr} The largest correlation (in absolute value) observed.
 #'
-#' \item{smallest_corr}{The smallest correlation (in absolute value) observed.}
+#' * \strong{smallest_corr} The smallest correlation (in absolute value)
+#' observed.
 #'
-#' \item{weight_var}{The variables with largest eigenvector (largest weight) in
-#' the eigenvalue of smallest value, sorted in decreasing order.}
+#' * \strong{weight_var} The variables with largest eigenvector (largest weight)
+#' in the eigenvalue of smallest value, sorted in decreasing order.
+#' @md
 #' @author Tiago Olivoto \email{tiagoolivoto@@gmail.com}
 #' @references Olivoto, T., V.Q. Souza, M. Nardino, I.R. Carvalho, M. Ferrari,
-#' A.J. Pelegrin, V.J. Szareski, and D. Schmidt. 2017. Multicollinearity in
-#' path analysis: a simple method to reduce its effects. Agron. J. 109:131-142.
-#' doi:10.2134/agronj2016.04.0196.
-#' \href{https://dl.sciencesocieties.org/publications/aj/abstracts/109/1/131}{doi:10.2134/agronj2016.04.0196}
+#'   A.J. Pelegrin, V.J. Szareski, and D. Schmidt. 2017. Multicollinearity in
+#'   path analysis: a simple method to reduce its effects. Agron. J.
+#'   109:131-142. doi:10.2134/agronj2016.04.0196.
+#'   \href{https://dl.sciencesocieties.org/publications/aj/abstracts/109/1/131}{doi:10.2134/agronj2016.04.0196}
+#'
+#' @references Olivoto, T., M. Nardino, I.I.R. Carvalho, D.N. Follmann, M.
+#'   Ferrari, A.J. de Pelegrin, V.J. Szareski, A.C. de Oliveira, B.O. Caron, and
+#'   V.Q. de Souza. 2017. Optimal sample size and data arrangement method in
+#'   estimating correlation matrices with lesser collinearity: A statistical
+#'   focus in maize breeding. African J. Agric. Res. 12:93-103.
+#'   \href{https://academicjournals.org/journal/AJAR/article-abstract/5DB554B62359}{doi:10.5897/AJAR2016.11799}.
+#'
 #' @export
 #' @examples
 #'
 #' # Using the correlation matrix
 #' library(metan)
+#'
 #' cor_iris <- cor(iris[,1:4])
 #' n <- nrow(iris)
-#' diag1 <- colindiag(cor_iris, n = n)
+#'
+#' col_diag <- colindiag(cor_iris, n = n)
 #'
 #'
-#' # Using a data frame, diagnostic by species, and using the pipe operator %>%
-#' col_diag_spec <- iris %>%
-#'                  split_factors(Species) %>%
-#'                  colindiag()
+#' # Using a data frame
+#' col_diag_gen <- data_ge2 %>%
+#'                 split_factors(GEN) %>%
+#'                 colindiag()
 #'
+#' # Diagnostic by levels of a factor selecting desired variables
+#' col_diag_gen <- data_ge2 %>%
+#'                 split_factors(GEN) %>%
+#'                 colindiag(EH, PH, CD, CL)
 #'
-colindiag <- function(.data, n = NULL, verbose = TRUE) {
-  if (!class(.data) %in% c("matrix", "data.frame", "split_factors",
-                           "covcor_design", "tbl_df")) {
+colindiag <- function(.data, ..., n = NULL, verbose = TRUE) {
+  if (!any(class(.data) %in% c("matrix", "data.frame", "split_factors",
+                           "covcor_design", "tbl_df"))) {
     stop("The object 'x' must be a correlation matrix, a data.frame or an object of class split_factors")
   }
   if (is.matrix(.data) && is.null(n)) {
@@ -139,45 +157,52 @@ colindiag <- function(.data, n = NULL, verbose = TRUE) {
                   largest_corr = largest_corr, smallest_corr = smallest_corr,
                   weight_var = pesovarname)
     if (verbose == TRUE) {
-      cat(paste0("Matrix determinant: ", round(Det, 7)),
-          "\n")
-      cat(paste0("Largest correlation: ", largest_corr),
-          "\n")
-      cat(paste0("Smallest correlation: ", smallest_corr),
-          "\n")
-      cat(paste0("Number of VIFs > 10: ", length(VIF >
-                                                   10)), "\n")
-      cat(paste0("Number of correlations with r >= |0.8|: ",
-                 ncorhigh), "\n")
-      cat(paste0("Variables with largest weight in the last eigenvalues: ",
-                 "\n", pesovarname), "\n")
+      cat(paste0("Matrix determinant: ", round(Det, 7)), "\n")
+      cat(paste0("Largest correlation: ", largest_corr), "\n")
+      cat(paste0("Smallest correlation: ", smallest_corr), "\n")
+      cat(paste0("Number of VIFs > 10: ", length(which(VIF > 10))), "\n")
+      cat(paste0("Number of correlations with r >= |0.8|: ", ncorhigh), "\n")
+      cat(paste0("Variables with largest weight in the last eigenvalues: \n", pesovarname), "\n")
     }
     return(invisible(final))
   }
+
   if (is.matrix(.data)) {
     out <- internal(.data)
   }
+
   if (any(class(.data) %in% c("split_factors", "covcor_design"))) {
-    out <- lapply(seq_along(.data[[1]]), function(x) {
+    if(!missing(...)){
+      dfs <- lapply(.data[[1]], function(x){
+        dplyr::select(x, ...)
+      })
+    } else{
+      dfs <- .data[[1]]
+    }
+    out <- lapply(seq_along(dfs), function(x) {
       if (verbose == TRUE) {
         cat("\n----------------------------------------------------------------------------\n")
-        cat("Level:", names(.data[[1]])[[x]], "\n")
+        cat("Level:", names(dfs)[[x]], "\n")
         cat("----------------------------------------------------------------------------\n")
       }
-      internal(.data[[1]][[x]])
+      internal(dfs[[x]])
     })
-    names(out) <- names(.data[[1]])
+    names(out) <- names(dfs)
   }
   if (is.data.frame(.data)) {
-    data <- .data[, unlist(lapply(.data, is.numeric))]
-    out <- internal(data)
-    if (verbose == TRUE) {
-      if (sum(lapply(.data, is.factor) == TRUE) > 0) {
-        message("The factors ", paste0(collapse = " ",
-                                       names(.data[, unlist(lapply(.data, is.factor))])),
-                " where excluded to perform the analysis. If you want to perform an analysis for each level of a factor, use the function 'split_factors() before.' ")
+    if(!missing(...)){
+      dfs <-  dplyr::select(.data, ...)
+    } else{
+      if (verbose == TRUE) {
+        if (sum(lapply(.data, is.factor) == TRUE) > 0) {
+          message("The factors ", paste0(collapse = " ",
+                                         names(.data[, unlist(lapply(.data, is.factor))])),
+                  " where excluded to perform the analysis. If you want to perform an analysis for each level of a factor, use the function 'split_factors() before.' ")
+        }
       }
+      dfs <- select_if(.data, is.numeric)
     }
+    out <- internal(dfs)
   }
   invisible(out)
 }
