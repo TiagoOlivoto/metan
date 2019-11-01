462a397d26df1e954475a4b1376495f402adc1f6
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-11-01 13:13:02 -0300

    Encode variables to a factor

---
+++ R/to_factor.R
@@ -0,0 +1,20 @@
+#' Encode variables to a factor
+#'
+#' Function to quick mutate columns to factor.
+#'
+#'
+#' @param .data A data frame
+#' @param ... The variable(s) to encode to a factor.
+#' @return An object of the same class of \code{.data} with the variables in
+#'   \code{...} encoded to a factor.
+#' @export
+#' @author Tiago Olivoto \email{tiagoolivoto@@gmail.com}
+#' @examples
+#'
+#' library(metan)
+#' PH_EH_to_factor <- to_factor(data_ge2, PH, EH)
+#' PH_EH_to_factor <- to_factor(data_ge2, 4:5)
+#'
+to_factor <- function(.data, ...){
+return(mutate_at(.data, vars(...), as.factor))
+}
