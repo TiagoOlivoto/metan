a0150cbe1eb1d99dd727c81eef046131eb318247
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-11-07 04:23:48 -0300

    Arrange ggplot objects

--- R/arrange_ggplot.R
+++ R/arrange_ggplot.R
@@ -1,10 +1,15 @@
 #' Arrange multiple ggplot2 graphics in a single image window
 #'
-#' This is a helper function to arrange ggplot2 objects in the metan package
+#' This is a helper function to arrange ggplot2 objects in the metan package. It
+#' imports \code{\link[cowplot]{plot_grid}}. For a complete usability use that
+#' function.
 #' @param ... An object of class \code{gg}
-#' @param nrow The number of rows
-#' @param ncol The number of columns
-#'
+#' @param plotlist List of plots to display.
+#' @param nrow,ncol The number of rows and columns, respectively.
+#' @param rel_widths,rel_heights The Numerical vector of relative columns widths
+#'   and rows heights, respectively.
+#' @param labels List of labels to be added to the plots.
+#' @param hjust,vjust Adjusts the horizontal and vertical position of each label.
 #' @return None.
 #' @export
 #'
@@ -18,7 +23,16 @@
 #'
 #' arrange_ggplot(p1, p2)
 #'
-arrange_ggplot <- function(..., nrow = NULL, ncol = NULL) {
-plot_grid(plotlist = ..., nrow = nrow, ncol = ncol)
+arrange_ggplot <- function(...,
+                           plotlist = NULL,
+                           nrow = NULL,
+                           ncol = NULL,
+                           rel_widths = 1,
+                           rel_heights = 1,
+                           labels = NULL,
+                           hjust = -0.5,
+                           vjust = 1.5) {
+plot_grid(plotlist = ..., plotlist = plotlist, nrow = nrow,
+          ncol = ncol, rel_widths = rel_widths, rel_heights = rel_heights,
+          labels = labels, hjust = hjust, vjust = vjust)
 }
-
