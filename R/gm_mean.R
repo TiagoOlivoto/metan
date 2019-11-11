7e3a37c446e6974a9ee6a142ff1d506751e63058
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-11-11 00:51:40 -0300

    Harmonic and geometric mean

---
+++ R/gm_mean.R
@@ -0,0 +1,33 @@
+#' Geometric mean
+#' Helper function to compute the geometric mean. The geometric mean is the nth
+#' root of n products.
+#' @param x A numeric vector or a data frame.
+#' @param ... Variables to compute the geometric mean. If no variable is informed
+#'   and \code{x} is a data frame, all the numeric variables will be used.
+#' @param na.rm A logical value indicating whether \code{NA} values should be
+#'   stripped before computation proceeds.
+#' @seealso \code{\link{hm_mean}}
+#' @note Not useful if there are elements with values \code{<=0}.
+#' @return The geometric mean(s) of \code{x}. If \code{x} is a numeric vector, the
+#'   function returns a numeric value. If a data frame is used then a numeric
+#'   vector with the geometric mean for each variable is returned.
+#' @export
+#'
+#' @examples
+#' num <- c(1:10, 50)
+#' gm_mean(num)
+#'
+#' num_df <- make_mat(data_ge, GEN, ENV, GY)
+#' gm_mean(num_df)
+gm_mean <- function(x, ..., na.rm = TRUE){
+  if(is.null(nrow(x))){
+    exp(sum(log(x[x > 0]), na.rm = na.rm) / length(x))
+  } else{
+    if(missing(...)){
+      df <- select_if(x, is.numeric)
+    } else{
+      df <- select(x, ...)
+    }
+    exp(apply(log(df), 2, mean, na.rm = na.rm))
+  }
+}
---
+++ R/hm_mean.R
@@ -0,0 +1,33 @@
+#' Harmonic mean
+#' Helper function to compute the harmonic mean. The harmonic mean is the
+#' reciprocal of the arithmetic mean of the reciprocals.
+#' @param x A numeric vector or a data frame.
+#' @param ... Variables to compute the harmonic mean. If no variable is informed
+#'   and \code{x} is a data frame, all the numeric variables will be used.
+#' @param na.rm A logical value indicating whether \code{NA} values should be
+#'   stripped before computation proceeds.
+#' @seealso \code{\link{gm_mean}}
+#' @note Not useful if there are elements with values \code{<=0}.
+#' @return The harmonic mean(s) of \code{x}. If \code{x} is a numeric vector, the
+#'   function returns a numeric value. If a data frame is used then a numeric
+#'   vector with the harmonic mean for each variable is returned.
+#' @export
+#'
+#' @examples
+#' num <- c(1:10, 50)
+#' hm_mean(num)
+#'
+#' num_df <- make_mat(data_ge, GEN, ENV, GY)
+#' hm_mean(num_df)
+hm_mean <- function(x, ..., na.rm = TRUE) {
+  if(is.null(nrow(x))){
+    1 / mean(1 / x, na.rm = na.rm)
+  } else{
+    if(missing(...)){
+      df <- select_if(x, is.numeric)
+    } else{
+      df <- dplyr::select(x, ...)
+    }
+    1 / (apply( 1 / df, 2, mean, na.rm = na.rm))
+  }
+}
