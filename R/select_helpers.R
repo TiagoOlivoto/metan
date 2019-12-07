3de0877f26a8c16484f6cb7c65deeb746e7a2190
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-12-07 20:14:37 -0300

    Select helpers

--- R/select_helpers.R
+++ R/select_helpers.R
@@ -34,47 +34,54 @@ NULL
 #' @param suffix A suffix that ends the variable name.
 #' @importFrom dplyr intersect
 #' @export
-#' @description These functions allow you to select variables based prefixes and suffixes.
-#' * \code{intersect_var()}: Select variables that starts with a prefix
-#' \strong{AND} ends wiht a suffix.
-#' * \code{union_var()}: Select variables that starts with a prefix \strong{OR}
-#' ends wiht a suffix.
+#' @description These functions allow you to select variables based operations
+#'   with prefixes and suffixes.
 #' * \code{difference_var()}: Select variables that starts with a prefix \strong{AND
 #' NOT} ends wiht a suffix.
+#'
+#' * \code{intersect_var()}: Select variables that starts with a prefix
+#' \strong{AND} ends wiht a suffix.
+#'
+#' * \code{union_var()}: Select variables that starts with a prefix \strong{OR}
+#' ends wiht a suffix.
+
 #' @md
 #' @examples
 #' library(metan)
+#'
+#' # Select variables that starts with "C" and not ends with "D".
+#' data_ge2 %>%
+#' select(difference_var("C", "D"))
+#'
 #' # Select variables that starts with "C" and ends with "D".
 #' data_ge2 %>%
 #' select(intersect_var("C", "D"))
 #'
-#' # Select variables that starts with "D" or ends with "D".
+#'
+#' # Select variables that starts with "C" or ends with "D".
 #' data_ge2 %>%
 #' select(union_var("C", "D"))
 #'
-#' # Select variables that starts with "D" and not ends with "D".
-#' data_ge2 %>%
-#' select(difference_var("C", "D"))
-#'
-intersect_var <- function(prefix, suffix) {
-  intersect(starts_with(prefix), ends_with(suffix))
+difference_var <- function(prefix, suffix) {
+  setdiff(starts_with(prefix), ends_with(suffix))
 }
-NULL
+
 
 #' @title Select helper
 #' @name Select_helper
 #' @keywords internal
 #' @importFrom dplyr union
 #' @export
-union_var <- function(prefix, suffix) {
-  union(starts_with(prefix), ends_with(suffix))
+intersect_var <- function(prefix, suffix) {
+  intersect(starts_with(prefix), ends_with(suffix))
 }
+
 
 #' @title Select helper
 #' @name Select_helper
 #' @keywords internal
 #' @importFrom dplyr setdiff
 #' @export
-difference_var <- function(prefix, suffix) {
-  setdiff(starts_with(prefix), ends_with(suffix))
+union_var <- function(prefix, suffix) {
+  union(starts_with(prefix), ends_with(suffix))
 }
