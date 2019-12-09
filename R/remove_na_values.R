4e51cf021330ef40b071501175c9c7c9b38f69f8
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-12-09 00:26:17 -0300

    remove columns or rows with NA values

---
+++ R/remove_na_values.R
@@ -0,0 +1,37 @@
+#' @title Remove NA values
+#' @name remove_na_values
+#' @param .data A data frame or tibble
+#' @param verbose Logical argument. If \code{TRUE} (default) shows in console
+#'   the rows or columns deleted.
+#' @export
+#' @return A data frame with rows or columns with \code{NA} values deleted.
+#' @description These functions allow you to remove rows or columns with
+#'   \code{NA} values quickly.
+#' * \code{remove_rows_na()}: Remove rows with \code{NA} values.
+#' * \code{remove_cols_na()}: Remove columns with \code{NA} values.
+#' @md
+#' @examples
+#' library(metan)
+#' data_with_na <- data_g
+#' data_with_na[c(1, 5, 10), c(3:5, 10:15)] <- NA
+#' data_with_na
+#' remove_cols_na(data_with_na)
+#' remove_rows_na(data_with_na)
+remove_rows_na <- function(.data, verbose = TRUE){
+row_with_na <- which(complete.cases(.data) ==  FALSE)
+if(verbose == TRUE){
+warning("Row(s) ", paste(row_with_na, collapse = ", "), " deleted.", call. = FALSE)
+}
+return(na.omit(.data))
+}
+
+#' @name remove_na_values
+#' @keywords internal
+#' @export
+remove_cols_na <- function(.data, verbose = TRUE){
+  cols_with_na <- names(which(sapply(.data, anyNA)))
+  if(verbose == TRUE){
+    warning("Column(s) ", paste(cols_with_na, collapse = ", "), " deleted.", call. = FALSE)
+  }
+  return(select(.data, -cols_with_na))
+}
