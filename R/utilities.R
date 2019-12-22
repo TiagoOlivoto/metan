e887f2891bfbf4f076f0a0aaba0fbb6894ff497b
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-12-22 17:09:52 -0300

    Utilities

--- R/utilities.R
+++ R/utilities.R
@@ -9,7 +9,11 @@ NULL
 #' @param ... Variables to round. If no variable is informed, all the numeric
 #'   variables from \code{data} are used.
 #' @param digits The number of significant figures.
-#' @param x A numeric value or a string. Vectors are also allowed.
+#' @param var The variable to extract or replace numbers or strings.
+#' @param new_var The name of the new variable containing the numbers or
+#'   strings extracted or replaced. Defaults to \code{new_var}.
+#' @param drop Logical argument. If \code{TRUE} keeps the new variable
+#'   \code{new_var} and drops the existing ones. Defaults to \code{FALSE}.
 #' @param pattern A string to be matched. Regular Expression Syntax is also
 #'   allowed.
 #' @param replacement A string for replacement.
@@ -39,40 +43,38 @@ NULL
 #'
 #' ########### Extract or replace numbers ##########
 #' # Extract numbers
-#' extract_number("GEN_10")
-#' extract_number(data_ge2$GEN)
+#' extract_number(data_ge, GEN)
+#' extract_number(data_ge,
+#'                var = GEN,
+#'                drop = TRUE,
+#'                new_var = g_number)
 #'
 #' mutate(data_ge,
 #'        GEN_NUMERIC = extract_number(GEN))
 #'
 #' # Replace numbers
-#' mutate(data_ge,
-#'        GEN_NUMERIC = replace_number(GEN))
 #'
-#' mutate(data_ge,
-#'        GEN_NUMERIC =
-#'          replace_number(GEN,
-#'                         pattern = "2",
-#'                         with = "_two"))
+#' replace_number(data_ge, GEN)
+#' replace_number(data_ge,
+#'                var = GEN,
+#'                pattern = "1",
+#'                replacement = "_one")
 #'
 #' ########## Extract or replace strings ##########
 #' # Extract strings
-#' extract_string("GEN_10")
-#' extract_string(data_ge2$GEN)
-#'
-#' mutate(data_ge,
-#'        GEN_STRING = extract_string(GEN))
+#' extract_string(data_ge, GEN)
+#' extract_string(data_ge,
+#'                var = GEN,
+#'                drop = TRUE,
+#'                new_var = g_name)
 #'
 #' # Replace strings
-#' mutate(data_ge,
-#'        GEN_STRING = replace_string(GEN))
-#' genotype <-
-#' mutate(data_ge,
-#'        GEN_CODE =
-#'          replace_string(GEN,
-#'                         pattern = "G",
-#'                         with = "GENOTYPE_"))
-#' genotype
+#' replace_string(data_ge, GEN)
+#' replace_string(data_ge,
+#'                var = GEN,
+#'                new_var = GENOTYPE,
+#'                pattern = "G",
+#'                replacement = "GENOTYPE_")
 #' ############# upper and lower cases ############
 #' all_lower_case("GENOTYPE")
 #' lc <- all_lower_case(data_ge)
@@ -83,7 +85,7 @@ NULL
 #' all_upper_case(lc)
 #' }
 #' @export
-#' @importFrom dplyr mutate_if
+#' @importFrom dplyr mutate_if transmute
 round_column <- function(.data, ...,  digits = 2){
   if (missing(...)){
     .data %<>% dplyr::mutate_if(is.numeric, round, digits = digits)
@@ -94,33 +96,81 @@ round_column <- function(.data, ...,  digits = 2){
 }
 #' @name utils-num-str
 #' @export
-extract_number <- function(x){
-  as.numeric(gsub("[^0-9.-]+", "", as.character(x)))
+extract_number <- function(.data,
+                           var,
+                           new_var = new_var,
+                           drop = FALSE){
+  if (drop == FALSE){
+    mutate(.data,
+           {{new_var}} :=   as.numeric(gsub("[^0-9.-]+", "", as.character({{var}})))
+    )
+  } else{
+    transmute(.data,
+              {{new_var}} :=   as.numeric(gsub("[^0-9.-]+", "", as.character({{var}})))
+    )
+  }
 }
 #' @name utils-num-str
 #' @export
-replace_number <- function(x, pattern = NULL, replacement = ""){
+replace_number <- function(.data,
+                           var,
+                           new_var = new_var,
+                           pattern = NULL,
+                           replacement = "",
+                           drop = FALSE){
   if(missing(pattern)){
     pattern <- "[0-9]"
   } else{
     pattern <- pattern
   }
-  gsub(pattern, replacement, as.character(x))
+  if (drop == FALSE){
+    mutate(.data,
+           {{new_var}} := gsub(pattern, replacement, as.character({{var}}))
+    )
+  } else{
+    transmute(.data,
+              {{new_var}} := gsub(pattern, replacement, as.character({{var}}))
+    )
+  }
 }
 #' @name utils-num-str
 #' @export
-extract_string <- function(x){
-  as.character(gsub("[^A-z.-]+", "", as.character(x)))
+extract_string <- function(.data,
+                           var,
+                           new_var = new_var,
+                           drop = FALSE){
+  if (drop == FALSE){
+    mutate(.data,
+           {{new_var}} := as.character(gsub("[^A-z.-]+", "", as.character({{var}})))
+    )
+  } else{
+    transmute(.data,
+              {{new_var}} := as.character(gsub("[^A-z.-]+", "", as.character({{var}})))
+    )
+  }
 }
 #' @name utils-num-str
 #' @export
-replace_string <- function(x, pattern = NULL, replacement = ""){
+replace_string <- function(.data,
+                           var,
+                           new_var = new_var,
+                           pattern = NULL,
+                           replacement = "",
+                           drop = FALSE){
   if(missing(pattern)){
     pattern <- "[A-z]"
   } else{
     pattern <- pattern
   }
-  gsub(pattern, replacement, as.character(x))
+  if (drop == FALSE){
+    mutate(.data,
+           {{new_var}} := gsub(pattern, replacement, as.character({{var}}))
+    )
+  } else{
+    transmute(.data,
+              {{new_var}} := gsub(pattern, replacement, as.character({{var}}))
+    )
+  }
 }
 #' @name utils-num-str
 #' @export
@@ -182,7 +232,7 @@ NULL
 #'   the values of those columns will be concatenated. Else, \code{col_1} or
 #'   \code{col_2} must be a numeric or a character vector of length equals to
 #'   the number of rows in \code{.data}. Vectors of length 1 will be recycled.
-#' @param var_name The name of the new variable containing the concatenated
+#' @param new_var The name of the new variable containing the concatenated
 #'   values from \code{col_1} and \code{col_2}. Defaults to \code{new_var}.
 #' @param sep The separator to be between values of \code{col_1} and
 #'   \code{col_2}. Defaults to "_".
@@ -223,7 +273,7 @@ NULL
 #' concatenate(data_ge,
 #'             col_1 = rep("ENV", 420),
 #'             col_2 = rep(1:14, each=30),
-#'             var_name = ENVIRONMENT,
+#'             new_var = ENVIRONMENT,
 #'             sep = "-")
 #'
 #' ################### Adding rows ##################
@@ -278,10 +328,10 @@ add_rows <- function(.data, ..., before = NULL, after = NULL){
 }
 #' @name utils-rows-cols
 #' @export
-concatenate <- function(.data, col_1, col_2, var_name = new_var,  sep = "_"){
+concatenate <- function(.data, col_1, col_2, new_var = new_var,  sep = "_"){
   .data %>%
     mutate(
-      {{var_name}} := paste({{col_1}}, sep, {{col_2}}, sep = "")
+      {{new_var}} := paste({{col_1}}, sep, {{col_2}}, sep = "")
     )
 }
 #' @name utils-rows-cols
