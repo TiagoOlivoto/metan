90f6d213b4dbf0adc995853fe140359fce47c60f
TiagoOlivoto <tiagoolivoto@gmail.com>
2019-12-27 16:12:16 -0300

    Use on.exit()

--- R/Schmildt.R
+++ R/Schmildt.R
@@ -152,8 +152,8 @@ print.Schmildt <- function(x, export = FALSE, file.name = NULL, digits = 3, ...)
   if (!class(x) == "Schmildt") {
     stop("The object must be of class 'Schmildt'")
   }
-  on.exit(options(options()))
-  options(pillar.sigfig = digits, ...)
+  opar <- options(pillar.sigfig = digits)
+  on.exit(options(opar))
   if (export == TRUE) {
     file.name <- ifelse(is.null(file.name) == TRUE, "Schmildt print", file.name)
     sink(paste0(file.name, ".txt"))
