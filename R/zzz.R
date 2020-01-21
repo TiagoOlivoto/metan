c0a5a05d254e766a811610d364eaaa97ef3fd055
TiagoOlivoto <tiagoolivoto@gmail.com>
2020-01-21 16:37:51 -0300

    New global variables added

--- R/zzz.R
+++ R/zzz.R
@@ -184,5 +184,6 @@ if (getRversion() >= "2.15.1") {
       "val", "Statistic", "Cluster", "CCP", "U1", "U2", "V1", "V2", "Var", "LEVEL",
       "GROUP", "Group", "model", "Variance (%)", "Xs", "Xo", "Pr(>Chisq)", "dat",
       "BLOCK", "rowid", "grank", "rMean", "rShukaVar", "value", "DF", "without", "RPGV_Y",
-      "HMRPGV_Y", "Pairs", "name", "v1", "v2", "slope", "new_var", "TYPE", "CODE"))
+      "HMRPGV_Y", "Pairs", "name", "v1", "v2", "slope", "new_var", "TYPE", "CODE",
+      "Sum.Sq", "Mean.Sq", "F.value", "Pr.F", "Sum Sq", "Df"))
 }
