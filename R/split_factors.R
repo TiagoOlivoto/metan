49888439f5d42254eef06600bb7203a3e6045bd8
TiagoOlivoto <tiagoolivoto@gmail.com>
2020-02-08 11:09:40 -0300

    All in split_factors.R

--- R/split_factors.R
+++ R/split_factors.R
@@ -2,23 +2,24 @@
 #'
 #' Split a data frame into subsets grouping by one or more factors.
 #'
-#' This function is used to split a data frame into a list where each element is
-#' a level of the grouping variable (or combination of grouping variables). By
-#' combining the function \code{split_factors} with the forward-pipe operator
-#' %>%, it is possible to apply some functions of the metan package to each
-#' element of the list.
-#'
+#' This function is used to split a data frame into a named list where each
+#' element is a level of the grouping variable (or combination of grouping
+#' variables).
+#' @name split_factors
 #' @param .data The data that will be split. Must contain at least one grouping
 #'   variable.
 #' @param ... Comma-separated list of unquoted variable names that will be used
-#'   to group the data.
-#' @param keep_factors If more than two factors are in the dataframe, should the
-#'   columns of the non-grouping factors be kept?
-#' @param verbose Logical argument. If \code{verbose = FALSE} the code will run
-#'   silently.
+#'   to split the data.
+#' @param keep_factors Should the grouping columns be kept?
+#' @param verbose Deprecated argument. It will be retired in the next release.
 #' @return A list where each element is a named level of the grouping factors.
 #'   If more than one grouping variable is used, then each element is the
 #'   combination of the grouping variables.
+#' @details
+#' * \code{split_factors()} Split a data frame by factors.
+#' * \code{as.splict_factors()} coerce to an object of class split_factors
+#' * \code{is.splict_factors()} check if an object is of class split_factors
+#'
 #' @author Tiago Olivoto \email{tiagoolivoto@@gmail.com}
 #' @export
 #' @importFrom rlang  eval_bare  expr set_names
@@ -28,28 +29,36 @@
 #'
 #' g1 <- split_factors(iris, Species)
 #' g2 <- split_factors(data_ge, ENV, keep_factors = TRUE)
+#'
+#' spdata <- as.split_factors(iris)
+#'
+#' is.split_factors(spdata)
 #' }
 #'
-split_factors <- function(.data, ..., keep_factors = FALSE, verbose = TRUE) {
+split_factors <- function(.data, ..., keep_factors = FALSE, verbose = "DEPRECATED") {
   grouped <- group_by(.data, ...)
   names <- eval_bare(expr(paste(!!!group_keys(grouped), sep = " | ")))
   gd <- grouped %>%
-    group_split(keep = TRUE) %>%
+    group_split(keep = keep_factors) %>%
     set_names(names)
-  if (keep_factors == FALSE) {
-    if (verbose == TRUE) {
-      if (sum(lapply(gd[[1]], is.factor) == TRUE) > 0) {
-        warning("Use 'keep_factors = TRUE' to keep the columns ", paste0(collapse = " ", names(gd[[1]][, unlist(lapply(gd[[1]], is.factor))])), " in the grouped data. ", call. = FALSE)
-      }
-    }
-    gd <- lapply(gd, function(x) {
-      x[, unlist(lapply(x, is.numeric))] %>% as_tibble()
-    })
-  } else {
-    gd <- lapply(gd, function(x) {
-      as_tibble(x)
-    })
+  return(gd %>% set_class("split_factors"))
+}
+#' @name split_factors
+#' @export
+as.split_factors <- function(x, keep_factors = FALSE, verbose = "DEPRECATED") {
+  grouped <- group_by_if(x, is.factor)
+  names <- eval_bare(expr(paste(!!!group_keys(grouped), sep = " | ")))
+  gd <- grouped %>%
+    group_split(keep = keep_factors) %>%
+    set_names(names)
+  return(gd %>% set_class("split_factors"))
+}
+#' @name split_factors
+#' @export
+is.split_factors <- function(x){
+  if(any(class(x) == "split_factors")){
+    return(TRUE)
+  } else{
+    return(FALSE)
   }
-  return(structure(list(dfs = gd, names = group_vars(grouped)),
-                   class = "split_factors"))
 }
