4149c4e41e49c23738de93f3e3f3e46ec9ebe676
TiagoOlivoto <34458429+TiagoOlivoto@users.noreply.github.com>
2020-05-14 23:55:04 -0300

    Update global variables

--- R/zzz.R
+++ R/zzz.R
@@ -189,5 +189,6 @@ if (getRversion() >= "2.15.1") {
       "BLUPge+e+re", "BLUPre", "Estimate", "HMRPGV", "RPGV", "Variance", "blup",
       "intercept", "lower", "upper", "pred_ols", "res_ammi", "res_ols", "pattern",
       "replacement", "comparison", "group1", "group2", "p.adj", "term", "rel_freq",
-      "variable", "Model", "level", "RESIDUAL", "MGIDI", "sd.amo"))
+      "variable", "Model", "level", "RESIDUAL", "MGIDI", "sd.amo", "SD", "SG", "data.x",
+      "data.y", "sense", "win"))
   }
