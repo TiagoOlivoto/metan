80e7e38c59979e0b6fe15be4423746153f63d71f
Tiago Olivoto <tiagoolivoto@gmail.com>
2022-07-04 09:59:42 -0300

    update ge_plot

--- R/ge_plot.R
+++ R/ge_plot.R
@@ -15,6 +15,15 @@
 #' @param resp The response variable.
 #' @param type The type of plot `type = 1` for a heatmap or `type = 2`
 #'   for a line plot.
+#' @param values Show the values in the plot? Defaults to `TRUE`.
+#' @param average Show the average values for environments and genotypes?
+#'   Defaults to `TRUE`.
+#' @param order_g,order_e A charactere vector indicating the order of the levels
+#'   for genotypes and environments, respectively. This can be used to change
+#'   the default ordering of rows and columns.
+#' @param xlab,ylab The labels for x and y axis, respectively.
+#' @param width_bar,heigth_bar The width and heigth of the legend bar,
+#'   respectively.
 #' @param plot_theme The graphical theme of the plot. Default is
 #'   `plot_theme = theme_metan()`. For more details,see
 #'   [ggplot2::theme()].
@@ -34,15 +43,74 @@ ge_plot <- function(.data,
                     gen,
                     resp,
                     type = 1,
+                    values = TRUE,
+                    average = TRUE,
+                    order_g = NULL,
+                    order_e = NULL,
+                    xlab = NULL,
+                    ylab = NULL,
+                    width_bar = 1.5,
+                    heigth_bar = 15,
                     plot_theme = theme_metan(),
                     colour = TRUE) {
   if(type == 1){
+    if(isTRUE(average)){
+      mat <-
+        select_cols(.data,
+                    ENV = {{env}},
+                    GEN = {{gen}},
+                    Y = {{resp}}) |>
+        make_mat(GEN, ENV, Y) |>
+        row_col_mean()
+
+      colnames(mat)[ncol(mat)] <- "Average"
+      rownames(mat)[nrow(mat)] <- "Average"
+      if(is.null(order_e)){
+        order_e <- colnames(mat)[-ncol(mat)]
+      } else{
+        order_e <- order_e
+      }
+      if(is.null(order_g)){
+        order_g <- rownames(mat)[-nrow(mat)]
+      } else{
+        order_g <- order_g
+      }
+      df_long <-
+        make_long(mat) |>
+        as_factor(1:2) |>
+        mutate(ENV = factor(ENV, levels = c(order_e, "Average")),
+               GEN = factor(GEN, levels = c("Average", order_g)))
+    } else{
+      df_long <-
+        select_cols(.data,
+                    ENV = {{env}},
+                    GEN = {{gen}},
+                    Y = {{resp}}) |>
+        means_by(ENV, GEN)
+      if(is.null(order_e)){
+        order_e <- levels(df_long$ENV)
+      } else{
+        order_e <- order_e
+      }
+      if(is.null(order_g)){
+        order_g <- levels(df_long$GEN)
+      } else{
+        order_g <- order_g
+      }
+      df_long <-
+        df_long |>
+        mutate(ENV = factor(ENV, levels = order_e),
+               GEN = factor(GEN, levels = order_g))
+    }
     p <-
-      ggplot(.data, aes({{env}}, {{gen}}, fill= {{resp}})) +
-      geom_tile()+
+      ggplot(df_long, aes(ENV, GEN, fill = Y)) +
+      geom_tile(color = "black")+
       scale_y_discrete(expand = expansion(mult = c(0,0)))+
       scale_x_discrete(expand = expansion(mult = c(0,0)))+
-      scale_fill_viridis_c()+
+      scale_fill_viridis_c() +
+      {if(isTRUE(values))geom_text(aes(label = round(Y, 1)),
+                                   color = "black",
+                                   size = 3)} +
       guides(fill = guide_colourbar(label = TRUE,
                                     draw.ulim = TRUE,
                                     draw.llim = TRUE,
@@ -50,38 +118,42 @@ ge_plot <- function(.data,
                                     ticks = TRUE,
                                     nbin = 10,
                                     label.position = "right",
-                                    barwidth = 1.3,
-                                    barheight = 10,
+                                    barwidth = width_bar,
+                                    barheight = heigth_bar,
                                     direction = 'vertical'))+
       plot_theme %+replace%
       theme(legend.position = "right",
-            legend.title = element_text())
+            legend.title = element_blank()) +
+      labs(x = xlab,
+           y = ylab)
 
   }
   if(type == 2){
-  p <- ggplot(.data, aes(x = {{env}}, y = {{resp}}))
-  if (colour == TRUE) {
-    p <- p +
-      stat_summary(aes(colour = {{gen}},
-                       group = {{gen}}),
-                   fun = mean,
-                   geom = "line",
-                   na.rm = TRUE)
-  } else {
-    p <- p +
-      stat_summary(aes(group = {{gen}}),
-                   fun = mean,
-                   geom = "line",
-                   colour = "black",
-                   na.rm = TRUE)
-  }
-  p <- p + geom_point(stat = "summary",
-                      fun = mean,
-                      size = 3,
-                      shape = 18) +
-    plot_theme %+replace%
-    theme(legend.position = "right")
+    p <- ggplot(.data, aes(x = {{env}}, y = {{resp}}))
+    if (colour == TRUE) {
+      p <- p +
+        stat_summary(aes(colour = {{gen}},
+                         group = {{gen}}),
+                     fun = mean,
+                     geom = "line",
+                     na.rm = TRUE)
+    } else {
+      p <- p +
+        stat_summary(aes(group = {{gen}}),
+                     fun = mean,
+                     geom = "line",
+                     colour = "black",
+                     na.rm = TRUE)
+    }
+    p <- p + geom_point(stat = "summary",
+                        fun = mean,
+                        size = 3,
+                        shape = 18) +
+      plot_theme %+replace%
+      theme(legend.position = "right")
   }
 
-  return(p)
+  return(p +
+           labs(x = xlab,
+                y = ylab))
 }
